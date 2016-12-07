5ac1c2532270e55141203e389f43c52b8a92b28c
Nicola Molinari <nicola.molinari@commercetools.de>
2016-12-07 22:22:11 +0100

    feat(http-middleware): implement http middleware

---
+++ packages/commercetools-sdk-middleware-http/test/http.spec.js
@@ -0,0 +1,183 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import nock from 'nock'
+import {
+  createHttpMiddleware,
+} from '../src'
+
+function createTestRequest (options) {
+  return {
+    url: '',
+    method: 'GET',
+    body: null,
+    headers: {},
+    ...options,
+  }
+}
+
+function createTestResponse (options) {
+  return {
+    ...options,
+  }
+}
+
+function createTestMiddlewareOptions (options) {
+  return {
+    host: 'https://api.commercetools.co',
+    ...options,
+  }
+}
+
+describe('Http', () => {
+  beforeEach(() => {
+    nock.cleanAll()
+  })
+
+  it('execute a get request (success)', () =>
+    new Promise((resolve, reject) => {
+      const request = createTestRequest({
+        url: '/foo/bar',
+      })
+      const response = createTestResponse({
+        resolve,
+        reject,
+      })
+      const next = (req, res) => {
+        expect(res).toEqual({
+          ...response,
+          body: { foo: 'bar' },
+          statusCode: 200,
+          headers: {
+            'content-type': ['application/json'],
+          },
+        })
+        resolve()
+      }
+      const middlewareOptions = createTestMiddlewareOptions()
+      const httpMiddleware = createHttpMiddleware(
+        middlewareOptions,
+      )
+      nock(middlewareOptions.host)
+        .defaultReplyHeaders({
+          'Content-Type': 'application/json',
+        })
+        .get('/foo/bar')
+        .reply(200, { foo: 'bar' })
+
+      httpMiddleware(next)(request, response)
+    }),
+  )
+
+  it('execute a post request (success)', () =>
+    new Promise((resolve, reject) => {
+      const request = createTestRequest({
+        url: '/foo/bar',
+        method: 'POST',
+        body: { hello: 'world' },
+      })
+      const response = createTestResponse({
+        resolve,
+        reject,
+      })
+      const next = (req, res) => {
+        expect(res).toEqual({
+          ...response,
+          body: { foo: 'bar' },
+          statusCode: 200,
+          headers: {
+            'content-type': ['application/json'],
+          },
+        })
+        resolve()
+      }
+      const middlewareOptions = createTestMiddlewareOptions()
+      const httpMiddleware = createHttpMiddleware(
+        middlewareOptions,
+      )
+      nock(middlewareOptions.host)
+        .defaultReplyHeaders({
+          'Content-Type': 'application/json',
+        })
+        .filteringRequestBody(() => '*')
+        .post('/foo/bar', '*')
+        .reply(200, { foo: 'bar' })
+
+      httpMiddleware(next)(request, response)
+    }),
+  )
+
+  it('handle failed response (network error)', () =>
+    new Promise((resolve, reject) => {
+      const request = createTestRequest({
+        url: '/foo/bar',
+      })
+      const response = createTestResponse({
+        resolve,
+        reject,
+      })
+      const next = (req, res) => {
+        expect(res.error.message).toBe(
+          // eslint-disable-next-line max-len
+          'request to https://api.commercetools.co/foo/bar failed, reason: oops',
+        )
+        expect(res.body).toBeUndefined()
+        expect(res.statusCode).toBeUndefined()
+        expect(res.headers).toBeUndefined()
+        resolve()
+      }
+      const middlewareOptions = createTestMiddlewareOptions()
+      const httpMiddleware = createHttpMiddleware(
+        middlewareOptions,
+      )
+      nock(middlewareOptions.host)
+        .defaultReplyHeaders({
+          'Content-Type': 'application/json',
+        })
+        .get('/foo/bar')
+        .replyWithError({ message: 'oops' })
+
+      httpMiddleware(next)(request, response)
+    }),
+  )
+
+  it('handle failed response (api error)', () =>
+    new Promise((resolve, reject) => {
+      const request = createTestRequest({
+        url: '/foo/bar',
+      })
+      const response = createTestResponse({
+        resolve,
+        reject,
+      })
+      const next = (req, res) => {
+        expect(res).toEqual({
+          ...response,
+          body: {
+            message: 'oops',
+            error: [{ code: 'InvalidField' }],
+          },
+          statusCode: 400,
+          headers: {
+            'content-type': ['application/json'],
+          },
+          error: new Error('oops'),
+        })
+        resolve()
+      }
+      const middlewareOptions = createTestMiddlewareOptions()
+      const httpMiddleware = createHttpMiddleware(
+        middlewareOptions,
+      )
+      nock(middlewareOptions.host)
+        .defaultReplyHeaders({
+          'Content-Type': 'application/json',
+        })
+        .get('/foo/bar')
+        .reply(400, {
+          message: 'oops',
+          error: [{ code: 'InvalidField' }],
+        })
+
+      httpMiddleware(next)(request, response)
+    }),
+  )
+})
