fafebcc93104cf55ed77f81016c9c91ea5352384
Nicola Molinari <nicola.molinari@commercetools.de>
2016-12-23 17:26:35 +0100

    build(travis): use release bot account for publishing [skip ci]

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -16,8 +16,8 @@ if [[ "$TRAVIS_BRANCH" == "master" ]]; then
   git config credential.helper "store --file=.git/credentials"
   echo "https://$RELEASE_GH_TOKEN:@github.com/$TRAVIS_REPO_SLUG.git" > .git/credentials
 
-  git config --global user.email "nicola.molinari@commercetools.de"
-  git config --global user.name "Nicola Molinari"
+  git config --global user.email "npmjs@commercetools.com"
+  git config --global user.name "CT Release Bot"
   git config --global push.default simple
 
   npm config set //registry.npmjs.org/:_authToken=${NPM_TOKEN} -q
