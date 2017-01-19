471be09eac84d5d95c2de13e320177869f5b37a4
Nicola Molinari <nicola.molinari@commercetools.de>
2017-01-19 21:40:16 +0100

    build(travis): fail fast and avoid running it tests for PRs coming from forks

---
+++ .travis/scripts.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+# This file runs all the main scripts to be run in the build.
+# Note: ensure to have `set -e` in order to fail the build fast.
+
+# 1. lint code
+npm run lint
+
+# 2. run tests + coverage
+npm run coverage:ci
+
+# 3. run integration tests
+# Note: PRs coming from forks do not have access to encrypted variables,
+# thus tasks that rely on that should not run.
+if [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
+  npm run it
+fi
+
+# 4. run dist tasks where necessary
+npm run dist
