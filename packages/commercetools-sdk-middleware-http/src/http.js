ed9aec431be9559667e32582f19e9f98a1c82d9f
Nicola Molinari <nicola.molinari@commercetools.de>
2017-01-21 13:09:11 +0100

    refactor(middleware-http): properly define default value

--- packages/commercetools-sdk-middleware-http/src/http.js
+++ packages/commercetools-sdk-middleware-http/src/http.js
@@ -18,12 +18,14 @@ import getErrorByCode, {
 const defaultApiHost = 'https://api.sphere.io'
 
 export default function createHttpMiddleware (
-  options: HttpMiddlewareOptions = {
-    host: defaultApiHost
-  },
+  options: HttpMiddlewareOptions,
 ): Middleware {
+  const {
+    host = defaultApiHost,
+  } = (options || {})
+
   return next => (request: ClientRequest, response: ClientResponse) => {
-    const url = options.host + request.uri
+    const url = host + request.uri
     const body = typeof request.body === 'string'
       ? request.body
       : JSON.stringify(request.body)
