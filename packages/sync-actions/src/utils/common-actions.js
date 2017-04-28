5d6cbc75195487c3ab3c1e5c0a54fba74ff08f37
lufego <luis.gomes@commercetools.de>
2017-04-28 16:57:15 +0200

    fix(common-actions): improves buildBaseAttributesActions function

--- packages/sync-actions/src/utils/common-actions.js
+++ packages/sync-actions/src/utils/common-actions.js
@@ -1,3 +1,4 @@
+import isNil from 'lodash.isnil'
 import clone from './clone'
 import * as diffpatcher from './diffpatcher'
 
@@ -24,18 +25,19 @@ export function buildBaseAttributesActions ({
       const delta = diff[key]
       const before = oldObj[key]
       const now = newObj[key]
-
+      const hasBefore = !isNil(oldObj[key])
+      const hasNow = !isNil(newObj[key])
       if (!delta) return undefined
 
-      if (!now && !before) return undefined
+      if (!hasNow && !hasBefore) return undefined
 
-      if (now && !before) // no value previously set
+      if (hasNow && !hasBefore) // no value previously set
         return { action: item.action, [actionKey]: now }
 
-      if (!now && !{}.hasOwnProperty.call(newObj, key)) // no new value
+      if (!hasNow && !{}.hasOwnProperty.call(newObj, key)) // no new value
         return undefined
 
-      if (!now && {}.hasOwnProperty.call(newObj, key)) // value unset
+      if (!hasNow && {}.hasOwnProperty.call(newObj, key)) // value unset
         return { action: item.action }
 
       // We need to clone `before` as `patch` will mutate it
