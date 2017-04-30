e742d5c7fbfa08338c92874e73618914e28ca829
wizzy25 <womayuku@gmail.com>
2017-04-30 12:58:00 +0200

    feat(discount-codes): implement function to generate codes
    affects: @commercetools/discount-code-generator

--- types/sdk.js
+++ types/sdk.js
@@ -154,3 +154,22 @@ export type ActionGroup = {
   type: string;
   group: 'black' | 'white';
 }
+
+/* Discount Codes */
+export type CodeData = {
+  name?: Object;
+  description?: Object;
+  cartDiscounts: [];
+  cartPredicate?: string;
+  isActive: boolean;
+  maxApplicationsPerCustomer?: number;
+  maxApplicationsPerCustomer?: number;
+  uri: string;
+  code?: string;
+}
+export type CodeDataArray = Array<CodeData>
+export type CodeOptions = {
+  quantity: number;
+  length?: number;
+  prefix?: string;
+}
