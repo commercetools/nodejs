1609d1f35109affb3b2f704672d875de12ced39a
wizzy25 <womayuku@gmail.com>
2017-05-03 13:46:20 +0200

    feat(discount-codes): cli module for I/O
    affects: @commercetools/discount-code-generator
    
    Implement CLI interface for passing configuration to the code generator

--- packages/discount-code-generator/src/cli.js
+++ packages/discount-code-generator/src/cli.js
@@ -2,8 +2,9 @@ import fs from 'fs'
 import npmlog from 'npmlog'
 import PrettyError from 'pretty-error'
 import yargs from 'yargs'
+import json2csv from 'json2csv'
 import csv from 'csv-parser'
-import { unflatten } from 'flat'
+import flatten, { unflatten } from 'flat'
 
 import discountCodeGenerator from './main'
 import { version } from '../package.json'
@@ -16,7 +17,7 @@ const args = yargs
 Usage: $0 [options]
 Generate multiple discount codes to import to the commercetools platform.`,
   )
-  .showHelpOnFail(true)
+  .showHelpOnFail(false)
 
   .option('help', {
     alias: 'h',
@@ -28,13 +29,6 @@ Generate multiple discount codes to import to the commercetools platform.`,
     type: 'boolean',
   })
   .version('version', 'Show version number.', version)
-
-  .option('inputFile', {
-    alias: 'i',
-    describe: 'Path to code options CSV or JSON file.',
-    demandOption: true,
-  })
-  .coerce('inputFile', arg => fs.createReadStream(String(arg)))
 
   .option('quantity', {
     alias: 'q',
@@ -56,29 +50,42 @@ Generate multiple discount codes to import to the commercetools platform.`,
     describe: 'Prefix for each code. No prefix will be used if omitted.',
   })
 
-  // TODO: uncomment this to work with output file
-  // .option('outputFile', {
-  //   alias: 'o',
-  //   default: 'stdout',
-  //   describe: 'Path to store generated output file.',
-  // })
-  // .coerce('outputFile', (arg) => {
-  //   if (arg !== 'stdout')
-  //     return fs.createWriteStream(String(arg))
+  .option('inputFile', {
+    alias: 'i',
+    describe: 'Path to code options CSV or JSON file.',
+    demandOption: true,
+  })
+  .coerce('inputFile', (arg) => {
+    if (arg.match(/\.json$/i) || arg.match(/\.csv$/i))
+      return fs.createReadStream(String(arg))
 
-  //   return process.stdout
-  // })
+    throw new Error('Invalid input file format. Must be CSV or JSON')
+  })
+
+  .option('outputFile', {
+    alias: 'o',
+    default: 'stdout',
+    describe: 'Path to store generated output file.',
+  })
+  .coerce('outputFile', (arg) => {
+    if (arg === 'stdout')
+      return process.stdout
+    if (arg.match(/\.json$/i) || arg.match(/\.csv$/i))
+      return fs.createWriteStream(String(arg))
+
+    throw new Error('Invalid output file format. Must be CSV or JSON')
+  })
 
   .option('delimiter', {
     alias: 'd',
     default: ',',
-    describe: 'Used CSV delimiter.',
+    describe: 'Used CSV delimiter for input and/or output file.',
   })
 
   .option('multivalueDelimiter', {
     alias: 'm',
     default: ';',
-    describe: 'Used CSV delimiter in multivalue fields (cartDiscounts).',
+    describe: 'Used CSV delimiter in multivalue fields for input/output file.',
   })
 
   .option('logLevel', {
@@ -91,13 +98,16 @@ Generate multiple discount codes to import to the commercetools platform.`,
   .argv
 
 // Resolve stream input to javascript object
-const resolveInput = (_args) => {
+export default function resolveInput (_args) {
   const input = _args.inputFile
   let _attributes
   return new Promise((resolve, reject) => {
     if (input.path.match(/\.json$/i))
       // Convert input stream to JSON file
       input
+        .on('error', (error) => {
+          reject(error)
+        })
         .on('data', (data) => {
           _attributes = data
         })
@@ -105,17 +115,69 @@ const resolveInput = (_args) => {
           resolve(JSON.parse(_attributes))
         })
     else if (input.path.match(/\.csv$/i))
-      input.pipe(csv({ separator: args.delimiter }))
+      // Convert input stream to CSV file
+      input.pipe(csv({
+        separator: _args.delimiter,
+        strict: true,
+      }))
+        .on('error', (error) => {
+          reject(error)
+        })
         .on('data', (data) => {
           const arrayDelim = _args.multivalueDelimiter
+          if (data.cartDiscounts)
           // eslint-disable-next-line no-param-reassign
-          data.cartDiscounts = data.cartDiscounts.split(arrayDelim)
+            data.cartDiscounts = data.cartDiscounts.split(arrayDelim)
           _attributes = data
         })
         .on('end', () => {
           resolve(unflatten(_attributes))
         })
-    else reject('Invalid file format')
+    else reject(new Error(
+      'Invalid file format, must be JSON or CSV',
+    ))
+  })
+}
+
+// Resove output to file or stdout
+const resolveOutput = (_args, outputData) => {
+  const outputStream = _args.outputFile
+  const total = outputData.length
+  return new Promise((resolve, reject) => {
+    if (outputStream === process.stdout) {
+      // Write to stdout
+      process.stdout.write(JSON.stringify(outputData, null, 1))
+      resolve(total)
+    } else if (outputStream.path.match(/\.json$/i)) {
+      // Write to json file
+      outputStream.on('error', (error) => {
+        reject(error)
+      })
+      outputStream.end(JSON.stringify(outputData, null, 1))
+      outputStream.on('finish', () => {
+        resolve(total)
+      })
+    } else if (outputStream.path.match(/\.csv$/i)) {
+      // Convert to csv and write to file
+      const arrayDelim = _args.multivalueDelimiter
+      const flatObjects = outputData.map((obj) => {
+        if (obj.cartDiscounts)
+        // eslint-disable-next-line no-param-reassign
+          obj.cartDiscounts = obj.cartDiscounts.join(arrayDelim)
+        return flatten(obj)
+      })
+      const csvOutput = json2csv({
+        data: flatObjects,
+        del: _args.delimiter,
+      })
+      outputStream.on('error', (error) => {
+        reject(error)
+      })
+      outputStream.end(csvOutput)
+      outputStream.on('finish', () => {
+        resolve(total)
+      })
+    } else reject(new Error('Invalid file format. Must be CSV or JSON'))
   })
 }
 
@@ -148,7 +210,9 @@ resolveInput(args)
   .then((attributes) => {
     const options = buildOptions(args)
     const codes = discountCodeGenerator(options, attributes)
-    // TODO: write generated codes to output file and remove console statement
-    console.log(codes)
+    return resolveOutput(args, codes)
+  })
+  .then((total) => {
+    process.stdout.write(`Successfully generated ${total} discount codes\n`)
   })
   .catch(errorHandler)
