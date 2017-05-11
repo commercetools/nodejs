f87550592877a787b85be27bba013c0dab12013d
Abimbola Idowu <abimbola2010@gmail.com>
2017-05-11 16:30:50 +0200

    fix(csv-parser-price): correct sku field
    affects: @commercetools/csv-parser-price

--- packages/csv-parser-price/test/main.spec.js
+++ packages/csv-parser-price/test/main.spec.js
@@ -52,7 +52,7 @@ describe('CsvParserPrice::parse', () => {
     const outputStream = streamtest['v2'].toText((error, result) => {
       const prices = JSON.parse(result).prices
       expect(prices.length).toBe(2)
-      expect(prices[0][CONSTANTS.header.sku]).toBeTruthy()
+      expect(prices[0].sku).toBeTruthy()
       done()
     })
     csvParserPrice.parse(readStream, outputStream)
@@ -73,7 +73,7 @@ describe('CsvParserPrice::parse', () => {
       expect(prices.length).toBe(2)
       expect(prices[0].prices.length).toBe(2)
       expect(prices[1].prices.length).toBe(1)
-      expect(prices[0][CONSTANTS.header.sku]).toBeTruthy()
+      expect(prices[0].sku).toBeTruthy()
       done()
     })
     csvParserPrice.parse(readStream, outputStream)
