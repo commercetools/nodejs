64ddabeab30e058ae1073e6449e73a30098f175b
Williams Omayuku <wizzy25@users.noreply.github.com>
2017-05-19 09:38:03 +0200

    feat(sync-actions): Add support for discount codes (#167)
    affects: @commercetools/sync-actions
    
    Add support for discount codes sync actions

---
+++ packages/sync-actions/src/discount-codes-actions.js
@@ -0,0 +1,25 @@
+import {
+  buildBaseAttributesActions,
+} from './utils/common-actions'
+
+export const baseActionsList = [
+  { action: 'changeIsActive', key: 'isActive' },
+  { action: 'setName', key: 'name' },
+  { action: 'setDescription', key: 'description' },
+  { action: 'setCartPredicate', key: 'cartPredicate' },
+  { action: 'setMaxApplications', key: 'maxApplications' },
+  {
+    action: 'setMaxApplicationsPerCustomer',
+    key: 'maxApplicationsPerCustomer',
+  },
+  { action: 'changeCartDiscounts', key: 'cartDiscounts' },
+]
+
+export function actionsMapBase (diff, oldObj, newObj) {
+  return buildBaseAttributesActions({
+    actions: baseActionsList,
+    diff,
+    oldObj,
+    newObj,
+  })
+}
