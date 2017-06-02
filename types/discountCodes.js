905058c4d2c06d4f4f4c4157920ca2a0c8322076
Williams Omayuku <wizzy25@users.noreply.github.com>
2017-06-02 13:06:41 +0200

    Discount Code importer (#171)
    Add new discount-code-importer module
    
    affects: @commercetools/discount-code-importer

---
+++ types/discountCodes.js
@@ -0,0 +1,50 @@
+/* @flow */
+import type { AuthMiddlewareOptions } from 'types/sdk'
+
+/* Discount Codes */
+export type CodeData = {
+  name?: Object;
+  description?: Object;
+  cartDiscounts: Array<string>;
+  cartPredicate?: string;
+  isActive: boolean;
+  maxApplications?: number;
+  maxApplicationsPerCustomer?: number;
+  code?: string;
+  id?: string;
+  version?: number;
+}
+
+export type CodeDataArray = Array<CodeData>
+
+export type CodeOptions = {
+  quantity: number;
+  length?: number;
+  prefix?: string;
+}
+
+export type Summary = {
+  created: number;
+  updated: number;
+  unchanged: number;
+  createErrorCount: number;
+  updateErrorCount: number;
+  errors: Array<any>;
+}
+
+/* Logger */
+export type LoggerOptions = {
+  error: () => mixed;
+  info: () => mixed;
+  warn: () => mixed;
+  verbose: () => mixed;
+}
+
+/* Config */
+export type ConstructorOptions = {
+  apiConfig: AuthMiddlewareOptions;
+  batchSize: number;
+  continueOnProblems?: boolean;
+}
+
+export type ChunkOptions = Array<Object>
