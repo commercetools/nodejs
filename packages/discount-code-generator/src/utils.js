f095413bf0d4cbd7dcbd4602e303484bd84e3897
wizzy25 <womayuku@gmail.com>
2017-06-15 16:03:02 +0200

    refactor(discount-code-gen): remove streaming from input, remove transform streams
    affects: @commercetools/discount-code-generator

--- packages/discount-code-generator/src/utils.js
+++ packages/discount-code-generator/src/utils.js
@@ -1,25 +1,37 @@
-import { Transform } from 'stream'
-
-// Implement a transform stream to correctly parse boolean and number types
-// from the CSV. This is necessary because the csv-parser parses all fields
-// as strings
-export class CsvTransform extends Transform {
-  constructor (typeCast = {}) {
-    super({ objectMode: true })
-    this.typeCast = typeCast
-  }
-
-  _transform (chunk, enc, callback) {
-    Object.keys(this.typeCast).forEach((field) => {
-      if (Object.prototype.hasOwnProperty.call(chunk, field))
-        // eslint-disable-next-line no-param-reassign
-        chunk[field] = this.typeCast[field](chunk[field])
-    })
-    this.push(chunk)
-    callback()
-  }
+// Implement a function to correctly cast boolean and number types from the CSV
+// input and handle the `cartDiscounts` field.
+// This is necessary because the csv-parser parses all fields as strings
+export default function prepareInput (object, arrayDelim) {
+  if (object.isActive)
+    // eslint-disable-next-line no-param-reassign
+    object.isActive = _parseBool(object.isActive)
+  if (object.maxApplications)
+    // eslint-disable-next-line no-param-reassign
+    object.maxApplications = parseInt(object.maxApplications, 10)
+  if (object.maxApplicationsPerCustomer)
+    // eslint-disable-next-line no-param-reassign
+    object.maxApplicationsPerCustomer = parseInt(
+      object.maxApplicationsPerCustomer, 10,
+    )
+  if (object.cartDiscounts)
+    // eslint-disable-next-line no-param-reassign
+    object.cartDiscounts = _formatCartDiscountInput(
+      object.cartDiscounts, arrayDelim,
+    )
+  return object
 }
 
-export function parseBool (value) {
+function _parseBool (value) {
   return value === 'true' || value === true
+}
+
+function _formatCartDiscountInput (cartDiscounts, arrayDelim) {
+  return cartDiscounts
+    .split(arrayDelim)
+    .map(cartDiscount => (
+      {
+        typeId: 'cart-discount',
+        id: cartDiscount,
+      }
+    ))
 }
