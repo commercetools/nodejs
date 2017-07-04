cc3acd38c173b0ce2428d6d212c6e1396f6d6afe
wizzy25 <womayuku@gmail.com>
2017-07-04 16:03:31 +0200

    feat(code-export): implement constructor, basic run
    affects: @commercetools/discount-code-exporter

--- packages/discount-code-exporter/test/main.spec.js
+++ packages/discount-code-exporter/test/main.spec.js
@@ -1,11 +1,49 @@
-import findSum from '../src/main'
+import DiscountCodeExport from '../src/main'
 
-describe('findSum', () => {
-  it('should be a function', () => {
-    expect(typeof findSum).toBe('function')
+describe('DiscountCodeExport', () => {
+  const logger = {
+    error: () => {},
+    warn: () => {},
+    info: () => {},
+    verbose: () => {},
+  }
+
+  let codeExport
+  beforeEach(() => {
+    codeExport = new DiscountCodeExport({
+      apiConfig: {
+        projectKey: 'asafaelhn',
+      },
+    }, logger)
   })
 
-  it('should return the sum of two numbers', () => {
-    expect(findSum(3, 5)).toBe(8)
+  describe('::constructor', () => {
+    it('should be a function', () => {
+      expect(typeof DiscountCodeExport).toBe('function')
+    })
+
+    it('should set default properties', () => {
+      expect(codeExport.apiConfig).toBeDefined()
+      expect(codeExport.batchSize).toBeDefined()
+      expect(codeExport.client).toBeDefined()
+      expect(codeExport.logger).toEqual(logger)
+    })
+
+
+    it('should throw if no `apiConfig` in `options` parameter', () => {
+      expect(() => new DiscountCodeExport({ foo: 'bar' })).toThrow(
+        /The contructor must be passed an `apiConfig` object/,
+      )
+    })
+
+    it('should throw if `batchSize` is more than 500', () => {
+      expect(() => new DiscountCodeExport({
+        apiConfig: {},
+        batchSize: 501,
+      }, logger))
+      .toThrow(
+        /The `batchSize` must not be more than 500/,
+      )
+    })
   })
 })
