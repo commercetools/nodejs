5a73077c799f43bb76dcd546ad9f376e24882355
Daniel Martínez <damarna85@gmail.com>
2017-08-24 12:58:31 +0200

    fix(cart-discounts-sync-actions): maintain tests pattern / export new sync actions in the index file

--- packages/sync-actions/test/cart-discounts-sync.spec.js
+++ packages/sync-actions/test/cart-discounts-sync.spec.js
@@ -3,7 +3,7 @@ import cartDiscountsSyncFn, {
 } from '../src/cart-discounts'
 import { baseActionsList } from '../src/cart-discounts-actions'
 
-describe('Exports', () => {
+describe('Cart Discounts Exports', () => {
   it('action group list', () => {
     expect(actionGroups).toEqual(['base'])
   })
@@ -24,266 +24,218 @@ describe('Exports', () => {
   })
 })
 
-describe('Actions', () => {
-  describe('changeIsActive action', () => {
-    let actual
-    let expected
-    beforeEach(() => {
-      const before = {
-        isActive: false,
-      }
+describe('Cart Discounts Actions', () => {
+  let cartDiscountsSync
+  beforeEach(() => { cartDiscountsSync = cartDiscountsSyncFn() })
 
-      const now = {
+  it('should build the `changeIsActive` action', () => {
+    const before = {
+      isActive: false,
+    }
+
+    const now = {
+      isActive: true,
+    }
+
+    const expected = [
+      {
+        action: 'changeIsActive',
         isActive: true,
-      }
-      actual = cartDiscountsSyncFn().buildActions(now, before)
-      expected = [
-        {
-          action: 'changeIsActive',
-          isActive: true,
-        },
-      ]
-    })
-
-    it('should build the `changeIsActive` action', () => {
-      expect(actual).toEqual(expected)
-    })
+      },
+    ]
+    const actual = cartDiscountsSync.buildActions(now, before)
+    expect(actual).toEqual(expected)
   })
-  describe('changeName action', () => {
-    let actual
-    let expected
-    beforeEach(() => {
-      const before = {
-        name: { en: 'en-name-before', de: 'de-name-before' },
-      }
 
-      const now = {
+  it('should build the `changeName` action', () => {
+    const before = {
+      name: { en: 'en-name-before', de: 'de-name-before' },
+    }
+
+    const now = {
+      name: { en: 'en-name-now', de: 'de-name-now' },
+    }
+
+    const expected = [
+      {
+        action: 'changeName',
         name: { en: 'en-name-now', de: 'de-name-now' },
-      }
-      actual = cartDiscountsSyncFn().buildActions(now, before)
-      expected = [
-        {
-          action: 'changeName',
-          name: { en: 'en-name-now', de: 'de-name-now' },
-        },
-      ]
-    })
-
-    it('should build the `changeName` action', () => {
-      expect(actual).toEqual(expected)
-    })
+      },
+    ]
+    const actual = cartDiscountsSync.buildActions(now, before)
+    expect(actual).toEqual(expected)
   })
-  describe('changeCartPredicate action', () => {
-    let actual
-    let expected
-    beforeEach(() => {
-      const before = {
-        cartPredicate: '1=1',
-      }
 
-      const now = {
+  it('should build the `changeCartPredicate` action', () => {
+    const before = {
+      cartPredicate: '1=1',
+    }
+
+    const now = {
+      cartPredicate: 'sku="test-sku"',
+    }
+
+    const expected = [
+      {
+        action: 'changeCartPredicate',
         cartPredicate: 'sku="test-sku"',
-      }
-      actual = cartDiscountsSyncFn().buildActions(now, before)
-      expected = [
-        {
-          action: 'changeCartPredicate',
-          cartPredicate: 'sku="test-sku"',
-        },
-      ]
-    })
-
-    it('should build the `changeCartPredicate` action', () => {
-      expect(actual).toEqual(expected)
-    })
+      },
+    ]
+    const actual = cartDiscountsSync.buildActions(now, before)
+    expect(actual).toEqual(expected)
   })
-  describe('changeSortOrder action', () => {
-    let actual
-    let expected
-    beforeEach(() => {
-      const before = {
-        sortOrder: '0.1',
-      }
 
-      const now = {
+  it('should build the `changeSortOrder` action', () => {
+    const before = {
+      sortOrder: '0.1',
+    }
+
+    const now = {
+      sortOrder: '0.2',
+    }
+
+    const expected = [
+      {
+        action: 'changeSortOrder',
         sortOrder: '0.2',
-      }
-      actual = cartDiscountsSyncFn().buildActions(now, before)
-      expected = [
-        {
-          action: 'changeSortOrder',
-          sortOrder: '0.2',
-        },
-      ]
-    })
-
-    it('should build the `changeSortOrder` action', () => {
-      expect(actual).toEqual(expected)
-    })
+      },
+    ]
+    const actual = cartDiscountsSync.buildActions(now, before)
+    expect(actual).toEqual(expected)
   })
-  describe('changeValue action', () => {
-    let actual
-    let expected
-    beforeEach(() => {
-      const before = {
-        value: {
-          type: 'relative',
-          permyriad: 100,
-        },
-      }
 
-      const now = {
+  it('should build the `changeValue` action', () => {
+    const before = {
+      value: {
+        type: 'relative',
+        permyriad: 100,
+      },
+    }
+
+    const now = {
+      value: {
+        type: 'relative',
+        permyriad: 200,
+      },
+    }
+
+    const expected = [
+      {
+        action: 'changeValue',
         value: {
           type: 'relative',
           permyriad: 200,
         },
-      }
-      actual = cartDiscountsSyncFn().buildActions(now, before)
-      expected = [
-        {
-          action: 'changeValue',
-          value: {
-            type: 'relative',
-            permyriad: 200,
-          },
-        },
-      ]
-    })
-
-    it('should build the `changeValue` action', () => {
-      expect(actual).toEqual(expected)
-    })
+      },
+    ]
+    const actual = cartDiscountsSync.buildActions(now, before)
+    expect(actual).toEqual(expected)
   })
-  describe('changeRequiresDiscountCode action', () => {
-    let actual
-    let expected
-    beforeEach(() => {
-      const before = {
-        requiresDiscountCode: false,
-      }
 
-      const now = {
+  it('should build the `changeRequiresDiscountCode` action', () => {
+    const before = {
+      requiresDiscountCode: false,
+    }
+
+    const now = {
+      requiresDiscountCode: true,
+    }
+
+    const expected = [
+      {
+        action: 'changeRequiresDiscountCode',
         requiresDiscountCode: true,
-      }
-      actual = cartDiscountsSyncFn().buildActions(now, before)
-      expected = [
-        {
-          action: 'changeRequiresDiscountCode',
-          requiresDiscountCode: true,
-        },
-      ]
-    })
-
-    it('should build the `changeRequiresDiscountCode` action', () => {
-      expect(actual).toEqual(expected)
-    })
+      },
+    ]
+    const actual = cartDiscountsSync.buildActions(now, before)
+    expect(actual).toEqual(expected)
   })
-  describe('changeTarget action', () => {
-    let actual
-    let expected
-    beforeEach(() => {
-      const before = {
-        target: {
-          type: 'customLineItems',
-          predicate: 'sku="sku-a"',
-        },
-      }
 
-      const now = {
+  it('should build the `changeTarget` action', () => {
+    const before = {
+      target: {
+        type: 'customLineItems',
+        predicate: 'sku="sku-a"',
+      },
+    }
+
+    const now = {
+      target: {
+        type: 'lineItems',
+        predicate: 'sku="sku-b"',
+      },
+    }
+
+    const expected = [
+      {
+        action: 'changeTarget',
         target: {
           type: 'lineItems',
           predicate: 'sku="sku-b"',
         },
-      }
-      actual = cartDiscountsSyncFn().buildActions(now, before)
-      expected = [
-        {
-          action: 'changeTarget',
-          target: {
-            type: 'lineItems',
-            predicate: 'sku="sku-b"',
-          },
-        },
-      ]
-    })
-
-    it('should build the `changeTarget` action', () => {
-      expect(actual).toEqual(expected)
-    })
+      },
+    ]
+    const actual = cartDiscountsSync.buildActions(now, before)
+    expect(actual).toEqual(expected)
   })
-  describe('setDescription action', () => {
-    let actual
-    let expected
-    beforeEach(() => {
-      const before = {
-        description: {
-          en: 'en-description-before',
-          de: 'de-description-before',
-        },
-      }
 
-      const now = {
+  it('should build the `setDescription` action', () => {
+    const before = {
+      description: {
+        en: 'en-description-before',
+        de: 'de-description-before',
+      },
+    }
+
+    const now = {
+      description: { en: 'en-description-now', de: 'de-description-now' },
+    }
+
+    const expected = [
+      {
+        action: 'setDescription',
         description: { en: 'en-description-now', de: 'de-description-now' },
-      }
-      actual = cartDiscountsSyncFn().buildActions(now, before)
-      expected = [
-        {
-          action: 'setDescription',
-          description: { en: 'en-description-now', de: 'de-description-now' },
-        },
-      ]
-    })
-
-    it('should build the `setDescription` action', () => {
-      expect(actual).toEqual(expected)
-    })
+      },
+    ]
+    const actual = cartDiscountsSync.buildActions(now, before)
+    expect(actual).toEqual(expected)
   })
-  describe('setValidFrom action', () => {
-    let actual
-    let expected
-    beforeEach(() => {
-      const before = {
-        validFrom: 'date1',
-      }
 
-      const now = {
+  it('should build the `setValidFrom` action', () => {
+    const before = {
+      validFrom: 'date1',
+    }
+
+    const now = {
+      validFrom: 'date2',
+    }
+
+    const expected = [
+      {
+        action: 'setValidFrom',
         validFrom: 'date2',
-      }
-      actual = cartDiscountsSyncFn().buildActions(now, before)
-      expected = [
-        {
-          action: 'setValidFrom',
-          validFrom: 'date2',
-        },
-      ]
-    })
-
-    it('should build the `setValidFrom` action', () => {
-      expect(actual).toEqual(expected)
-    })
+      },
+    ]
+    const actual = cartDiscountsSync.buildActions(now, before)
+    expect(actual).toEqual(expected)
   })
-  describe('setValidUntil action', () => {
-    let actual
-    let expected
-    beforeEach(() => {
-      const before = {
-        validUntil: 'date1',
-      }
 
-      const now = {
+  it('should build the `setValidUntil` action', () => {
+    const before = {
+      validUntil: 'date1',
+    }
+
+    const now = {
+      validUntil: 'date2',
+    }
+
+    const expected = [
+      {
+        action: 'setValidUntil',
         validUntil: 'date2',
-      }
-      actual = cartDiscountsSyncFn().buildActions(now, before)
-      expected = [
-        {
-          action: 'setValidUntil',
-          validUntil: 'date2',
-        },
-      ]
-    })
-
-    it('should build the `setValidUntil` action', () => {
-      expect(actual).toEqual(expected)
-    })
+      },
+    ]
+    const actual = cartDiscountsSync.buildActions(now, before)
+    expect(actual).toEqual(expected)
   })
 })
