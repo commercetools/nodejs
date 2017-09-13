e2afe79a45d65477a40f8e2b22fc20d831c26bee
wizzy25 <womayuku@gmail.com>
2017-09-13 17:16:36 +0200

    feat(json-product-parser): initial commit
    affects: @commercetools/json-parser-products

---
+++ packages/json-parser-products/src/main.js
@@ -0,0 +1,3 @@
+export default function addTwoNumbers (a, b) {
+  return a + b
+}
---
+++ packages/json-parser-products/test/main.spec.js
@@ -0,0 +1,11 @@
+import addTwoNumbers from '../src/main'
+
+describe('findSum', () => {
+  it('should be a function', () => {
+    expect(typeof addTwoNumbers).toBe('function')
+  })
+
+  it('should return the sum of two numbers', () => {
+    expect(addTwoNumbers(3, 5)).toBe(8)
+  })
+})
