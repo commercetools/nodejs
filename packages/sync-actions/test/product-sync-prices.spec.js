cdb4310981061875197a7e7e940b8954258e61aa
wizzy25 <womayuku@gmail.com>
2017-10-09 13:16:39 +0200

    chore(dependencies): update jest and refactor test
    affects: @commercetools/sync-actions
    
    As of v21, jest no longer supports nesting a test case in another test case. This commit refactors a
    test on sync-actions to remove test nesting.

--- packages/sync-actions/test/product-sync-prices.spec.js
+++ packages/sync-actions/test/product-sync-prices.spec.js
@@ -7,7 +7,7 @@ describe('Actions', () => {
     productsSync = productsSyncFn()
   })
 
-  it('should build actions for prices', () => {
+  it('build actions for prices and not delete the discounted field from original object', () => {
     const validFrom = new Date().toISOString()
     const discounted = { value: { centAmount: 4000, currencyCode: 'EUR' }, discount: { typeId: 'product-discount', id: 'pd1' } }
 
@@ -83,11 +83,9 @@ describe('Actions', () => {
       { action: 'removePrice', priceId: '222' },
       { action: 'addPrice', variantId: 3, price: { value: { currencyCode: 'USD', centAmount: 5000 }, country: 'US', customerGroup: { typeId: 'customer-group', id: 'cg1' }, channel: { typeId: 'channel', id: 'ch1' }, validFrom } },
     ])
-
-    it('should not delete the discounted field from the original object', () => {
-      expect('discounted' in before.masterVariant.prices[0]).toBeTruthy()
-      expect('discounted' in now.masterVariant.prices[0]).toBeTruthy()
-    })
+    // should not delete the discounted field from the original object
+    expect('discounted' in before.masterVariant.prices[0]).toBeTruthy()
+    expect('discounted' in now.masterVariant.prices[0]).toBeTruthy()
   })
 
   it('should not build actions if prices are not set', () => {
