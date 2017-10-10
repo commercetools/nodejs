cf0d18acfbdb07c1f01faa234b41f9e9fc2748bc
Williams Omayuku <wizzy25@users.noreply.github.com>
2017-10-10 13:23:12 +0200

    refactor(sync-actions): refactor test

--- packages/sync-actions/test/product-sync-prices.spec.js
+++ packages/sync-actions/test/product-sync-prices.spec.js
@@ -7,7 +7,7 @@ describe('Actions', () => {
     productsSync = productsSyncFn()
   })
 
-  it('build actions for prices and not delete the discounted field from original object', () => {
+  describe('build actions', () => {
     const validFrom = new Date().toISOString()
     const discounted = { value: { centAmount: 4000, currencyCode: 'EUR' }, discount: { typeId: 'product-discount', id: 'pd1' } }
 
@@ -79,15 +79,19 @@ describe('Actions', () => {
       ],
     }
 
-    const actions = productsSync.buildActions(now, before)
-    expect(actions).toEqual([
-      { action: 'changePrice', priceId: '111', price: { id: '111', value: { currencyCode: 'EUR', centAmount: 2000 }, country: 'US' } },
-      { action: 'removePrice', priceId: '222' },
-      { action: 'addPrice', variantId: 3, price: { value: { currencyCode: 'USD', centAmount: 5000 }, country: 'US', customerGroup: { typeId: 'customer-group', id: 'cg1' }, channel: { typeId: 'channel', id: 'ch1' }, validFrom } },
-    ])
-    // should not delete the discounted field from the original object
-    expect('discounted' in before.masterVariant.prices[0]).toBeTruthy()
-    expect('discounted' in now.masterVariant.prices[0]).toBeTruthy()
+    it('should build actions for prices', () => {
+      const actions = productsSync.buildActions(now, before)
+      expect(actions).toEqual([
+        { action: 'changePrice', priceId: '111', price: { id: '111', value: { currencyCode: 'EUR', centAmount: 2000 }, country: 'US' } },
+        { action: 'removePrice', priceId: '222' },
+        { action: 'addPrice', variantId: 3, price: { value: { currencyCode: 'USD', centAmount: 5000 }, country: 'US', customerGroup: { typeId: 'customer-group', id: 'cg1' }, channel: { typeId: 'channel', id: 'ch1' }, validFrom } },
+      ])
+    })
+
+    it('should not delete the discounted field from the original object', () => {
+      expect('discounted' in before.masterVariant.prices[0]).toBeTruthy()
+      expect('discounted' in now.masterVariant.prices[0]).toBeTruthy()
+    })
   })
 
   it('should not build actions if prices are not set', () => {
