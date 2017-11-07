13b407cb1b254411b114336fc3be1409904133ee
tdeekens <nerd@tdeekens.name>
2017-11-07 18:02:21 +0100

    chore(rollup): reformat rollup without semicolons

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,15 @@
 /* eslint-disable */
-const resolve = require('rollup-plugin-node-resolve');
-const commonjs = require('rollup-plugin-commonjs');
-const json = require('rollup-plugin-json');
-const babel = require('rollup-plugin-babel');
-const replace = require('rollup-plugin-replace');
-const uglify = require('rollup-plugin-uglify');
-const filesize = require('rollup-plugin-filesize');
+const resolve = require('rollup-plugin-node-resolve')
+const commonjs = require('rollup-plugin-commonjs')
+const json = require('rollup-plugin-json')
+const babel = require('rollup-plugin-babel')
+const replace = require('rollup-plugin-replace')
+const uglify = require('rollup-plugin-uglify')
+const filesize = require('rollup-plugin-filesize')
 /* eslint-enable */
 
-const env = process.env.NODE_ENV;
-const version = process.env.npm_package_version;
+const env = process.env.NODE_ENV
+const version = process.env.npm_package_version
 
 const config = {
   sourcemap: true,
@@ -31,7 +31,7 @@ const config = {
     commonjs(),
     filesize(),
   ],
-};
+}
 
 if (env === 'production') {
   config.plugins.push(
@@ -40,7 +40,7 @@ if (env === 'production') {
         warnings: false,
       },
     })
-  );
+  )
 }
 
-module.exports = config;
+module.exports = config
