536b21fdcd49d18295d61252054ecf8d0c99f59f
Dominik Ferber <dominik.ferber@gmail.com>
2017-11-07 18:37:46 +0100

    test(api-request-builder): add failing test-cases
    affects: @commercetools/api-request-builder

--- packages/api-request-builder/test/create-service.spec.js
+++ packages/api-request-builder/test/create-service.spec.js
@@ -95,6 +95,31 @@ describe('createService', () => {
       features: ['queryOne', 'queryExpand'],
     }
 
+    describe('with additional services', () => {
+      let service
+      beforeEach(() => {
+        service = createService({
+          type: 'test',
+          endpoint: '/test',
+          features: ['queryOne', 'queryExpand'],
+        }, projectKey)
+      })
+      it('should mix customerId and queryParams', () => {
+        expect(service.byCustomerId('foo').expand('baz').build())
+        .toBe('/my-project1/test/?customerId=foo&expand=baz')
+      })
+
+      it('should mix customerId and version', () => {
+        expect(service.byCustomerId('foo').withVersion(3).build())
+        .toBe('/my-project1/test/?customerId=foo&version=3')
+      })
+
+      it('should mix queryParams and version', () => {
+        expect(service.withVersion(3).expand('baz').build())
+        .toBe('/my-project1/test?expand=baz&version=3')
+      })
+    })
+
     it('include projectkey in uri by default', () => {
       expect(createService(options, projectKey)
         .build())
