3b92cb166c695eed2ad6420f5b70599bd6cd0b12
tdeekens <nerd@tdeekens.name>
2017-11-07 18:10:11 +0100

    chore(package): add plugin for globals and buildins to rollup

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,8 @@ const json = require('rollup-plugin-json')
 const babel = require('rollup-plugin-babel')
 const replace = require('rollup-plugin-replace')
 const uglify = require('rollup-plugin-uglify')
+const builtins = require('rollup-plugin-node-builtins')
+const globals = require('rollup-plugin-node-globals')
 const filesize = require('rollup-plugin-filesize')
 /* eslint-enable */
 
@@ -14,21 +16,25 @@ const version = process.env.npm_package_version
 const config = {
   sourcemap: true,
   plugins: [
+    json(),
     replace({
       'process.env.NODE_ENV': JSON.stringify(env),
       VERSION: `'${version}'`,
     }),
+    globals(),
+    builtins(),
+    resolve({
+      module: true,
+      jsnext: true,
+      main: true,
+      preferBuiltins: true,
+    }),
+    commonjs(),
     babel({
       babelrc: true,
       exclude: 'node_modules/**',
       runtimeHelpers: true,
     }),
-    json(),
-    resolve({
-      module: true,
-      preferBuiltins: true,
-    }),
-    commonjs(),
     filesize(),
   ],
 }
