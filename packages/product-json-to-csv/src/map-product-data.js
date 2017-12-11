688ce4eb36e5fe6db8f3270a480c3b5e59f20291
Williams Omayuku <wizzy25@users.noreply.github.com>
2017-12-11 17:46:12 +0100

    292 json2csv (#326)
    implement core parsing functionality:
     - resolve references
     - arrange data shape
     - write to file(s)

---
+++ packages/product-json-to-csv/src/map-product-data.js
@@ -0,0 +1,197 @@
+/* @flow */
+import type {
+  ResolvedProductProjection,
+  ProdWithMergedVariants,
+  SingleVarPerProduct,
+  MappedProduct,
+  Variant,
+  Category,
+  Image,
+} from 'types/product'
+
+import { flatten } from 'flat'
+import { isEmpty, reduce, pickBy } from 'lodash'
+
+export default class ProductMapping {
+  // Set flowtype annotations
+  fillAllRows: boolean
+  categoryBy: 'name' | 'key' | 'externalId' | 'namedPath'
+  lang: string
+  multiValDel: string
+
+  constructor(
+    {
+      fillAllRows = false,
+      categoryBy = 'name',
+      lang = 'en',
+      multiValueDelimiter = ';',
+    }: Object = {}
+  ) {
+    this.fillAllRows = fillAllRows
+    this.categoryBy = categoryBy
+    this.lang = lang
+    this.multiValDel = multiValueDelimiter
+  }
+
+  run(product: ResolvedProductProjection) {
+    const mergedVarProduct = ProductMapping._mergeVariants(product)
+    const varWithProductInfo = ProductMapping._spreadProdOnVariants(
+      mergedVarProduct,
+      this.fillAllRows
+    )
+    const mappedProduct = varWithProductInfo.map(
+      (variant: SingleVarPerProduct) => this._mapProperties(variant)
+    )
+    const flatProducts = mappedProduct.map(flatten)
+    return flatProducts
+  }
+
+  // merge all variants into a `variant` property and remove
+  // `masterVariant` and `variants` fields from product
+  static _mergeVariants(
+    product: ResolvedProductProjection
+  ): ProdWithMergedVariants {
+    const variant = [product.masterVariant, ...product.variants]
+    const { masterVariant, variants, ...restProduct } = product
+    return { ...restProduct, variant }
+  }
+
+  static _spreadProdOnVariants(
+    product: ProdWithMergedVariants,
+    fillAllRows: boolean
+  ): Array<SingleVarPerProduct> {
+    if (fillAllRows)
+      return product.variant.map((eachVariant: Variant): Object => ({
+        ...product,
+        variant: eachVariant,
+      }))
+
+    const productWithVariants: Array<Object> = product.variant.map(
+      (eachVariant: Variant): SingleVarPerProduct => ({
+        variant: eachVariant,
+      })
+    )
+    productWithVariants[0] = { ...product, ...productWithVariants[0] }
+    return productWithVariants
+  }
+
+  _mapProperties(product: SingleVarPerProduct): MappedProduct {
+    return reduce(
+      product,
+      (acc: Object, value: any, property: string): Object => {
+        switch (property) {
+          case 'productType': {
+            acc[property] = value.name
+            break
+          }
+          case 'state': {
+            acc[property] = value.key
+            break
+          }
+          case 'taxCategory': {
+            acc[property] = value.name
+            break
+          }
+          case 'categories': {
+            acc[property] = ProductMapping._mapCategories(
+              value,
+              this.categoryBy,
+              this.multiValDel,
+              this.lang
+            )
+            break
+          }
+          case 'categoryOrderHints': {
+            if (!isEmpty(value))
+              acc[property] = Object.keys(value)
+                .map((key: string): string => `${key}:${value[key]}`)
+                .join(this.multiValDel)
+            break
+          }
+          case 'searchKeywords': {
+            if (!isEmpty(value)) {
+              acc[property] = {}
+              Object.keys(value).forEach(language => {
+                const standard = []
+                const whitespace = []
+                value[language].forEach(keyWord => {
+                  if (!keyWord.suggestTokenizer) standard.push(keyWord.text)
+                  else if (keyWord.suggestTokenizer.type === 'whitespace')
+                    whitespace.push(keyWord.text.replace(/ /g, ' | '))
+                })
+                acc[property][language] = [...standard, ...whitespace].join(';')
+              })
+            }
+            break
+          }
+          case 'version': {
+            // Remove version number
+            break
+          }
+          case 'variant': {
+            if (!isEmpty(value.attributes)) {
+              value.attributes.forEach((attribute: Object) => {
+                acc[attribute.name] = attribute.value.key || attribute.value
+              })
+
+              // check if product is masterVariant
+              if (product.productType)
+                product.productType.attributes.forEach((attribute: Object) => {
+                  if (!acc[attribute.name]) acc[attribute.name] = ''
+                })
+            }
+            let images
+            if (!isEmpty(value.images))
+              images = value.images
+                .map((image: Image): string => {
+                  const { url, dimensions, label } = image
+                  let imageString = `${url}|${dimensions.w}|${dimensions.h}`
+                  imageString += label ? `|${label}` : ''
+                  return imageString
+                })
+                .join(';')
+
+            const { id, sku, key } = value
+            acc[property] = pickBy({ id, sku, key, images }, Boolean)
+            break
+          }
+          default: {
+            acc[property] = value
+          }
+        }
+        return acc
+      },
+      {}
+    )
+  }
+
+  static _mapCategories(
+    categories: Array<Category>,
+    categoryBy: string,
+    multiValDel: string,
+    lang: string
+  ): string {
+    if (categoryBy === 'name')
+      // name is localized
+      return categories
+        .map((cat: Category): string => cat[categoryBy][lang])
+        .join(multiValDel)
+    else if (categoryBy === 'externalId' || categoryBy === 'key')
+      return categories
+        .map((cat: Category): string => cat[categoryBy])
+        .join(multiValDel)
+    return categories
+      .map((cat: Category): string =>
+        ProductMapping._retrieveNamedPath(cat, lang)
+      )
+      .join(multiValDel)
+  }
+
+  static _retrieveNamedPath(category: Category, lang: string): string {
+    const getParent = (cat: Object): string => {
+      if (!cat.parent) return cat.name[lang]
+      return `${getParent(cat.parent)}>${cat.name[lang]}`
+    }
+    return getParent(category)
+  }
+}
---
+++ packages/product-json-to-csv/test/writer.spec.js
@@ -0,0 +1,181 @@
+import fs from 'fs'
+import highland from 'highland'
+import tmp from 'tmp'
+import StreamTest from 'streamtest'
+import unzip from 'unzip'
+import * as writer from '../src/writer'
+
+const streamTest = StreamTest.v2
+
+describe('Writer', () => {
+  let logger
+  let sampleProducts
+  beforeEach(() => {
+    logger = { info: jest.fn() }
+    sampleProducts = [
+      {
+        id: '12345ab-id',
+        key: 'product-key',
+        productType: 'product-type-1',
+        'variant.id': 1,
+        'variant.sku': 'A0E200000001YKI',
+        'variant.images': 'image-url1',
+        addedAttr: '',
+        anotherAddedAttr: '',
+        article: 'sample 089 WHT',
+        color: 'white',
+        'colorFreeDefinition.en': 'black-white',
+      },
+      {
+        'variant.id': 6,
+        'variant.sku': 'A0E200001YKI123',
+        'variant.images': 'https://image1.de;myImage.com',
+        article: 'sample 089 WHT',
+        color: 'white',
+        'colorFreeDefinition.en': 'black-white',
+      },
+      {
+        id: '75345ab-id',
+        key: 'product-key-2',
+        productType: 'product-type-2',
+        'variant.id': 1,
+        'variant.sku': 'A0E200000001YKI',
+        'variant.images': 'https://example.com/foobar/commer.jpg|3|4',
+        addedAttr: '',
+        anotherAddedAttr: '',
+        article: 'sample 089 WHT',
+        color: 'white',
+        'colorFreeDefinition.en': 'black-white',
+        'colorFreeDefinition.de': 'schwarz-weiß',
+        designer: 'michaelkors',
+      },
+      {
+        'variant.id': 3,
+        'variant.sku': 'A0E200001YKI123',
+        'variant.images': 'https://example.com/foobar/commer234.jpg|3|3',
+        'colorFreeDefinition.de': 'schwarz-weiß',
+        designer: 'michaelkors',
+      },
+      {
+        id: 'another5ab-id',
+        key: 'product-key',
+        productType: 'product-type-1',
+        'variant.id': 1,
+        'variant.sku': 'A0E200001YKI',
+        addedAttr: '',
+        anotherAddedAttr: '',
+        article: 'sample 777 WHT',
+        color: 'grune',
+        'colorFreeDefinition.en': 'grey-white',
+      },
+      {
+        'variant.id': 21,
+        'variant.sku': 'PPP0001YKI123',
+        'variant.images': 'https://eg993',
+        'colorFreeDefinition.en': 'schwarz-weiß',
+        color: 'blau',
+      },
+    ]
+  })
+
+  describe('::writeToSingleCsvFile', () => {
+    it('write products to a single file with specified headers', done => {
+      const sampleStream = highland(sampleProducts)
+      const headers = [
+        'id',
+        'key',
+        'productType',
+        'variantId',
+        'sku',
+        'images',
+        'anotherAddedAttr',
+        'article',
+        'designer',
+        'color',
+      ]
+      const outputStream = streamTest.toText((error, actual) => {
+        expect(true).toBeTruthy()
+        expect(error).toBeFalsy()
+        const expectedCsvFile = `${__dirname}/helpers/csvFileWithHeaders.csv`
+        const expectedCsv = fs.readFileSync(expectedCsvFile, 'utf8')
+        expect(expectedCsv).toMatch(actual)
+        done()
+      })
+
+      writer.writeToSingleCsvFile(sampleStream, outputStream, logger, headers)
+    })
+
+    it('log success info on completion', done => {
+      const sampleStream = highland(sampleProducts)
+      const headers = []
+      const outputStream = streamTest.toText(() => {})
+      outputStream.on('finish', () => {
+        expect(logger.info).toBeCalledWith(
+          expect.stringMatching(/written to CSV file/)
+        )
+        done()
+      })
+
+      writer.writeToSingleCsvFile(sampleStream, outputStream, logger, headers)
+    })
+  })
+
+  describe('::writeToZipFile', () => {
+    it('write products to multiple files based on productTypes', done => {
+      const sampleStream = highland(sampleProducts)
+      const verifyCsv1 = streamTest.toText((error, actual) => {
+        expect(error).toBeFalsy()
+        const type1 = `${__dirname}/helpers/productType1Sample.csv`
+        const expectedCsv = fs.readFileSync(type1, 'utf8')
+        expect(expectedCsv).toMatch(actual)
+      })
+      const verifyCsv2 = streamTest.toText((error, actual) => {
+        expect(error).toBeFalsy()
+        const type2 = `${__dirname}/helpers/productType2Sample.csv`
+        const expectedCsv = fs.readFileSync(type2, 'utf8')
+        expect(expectedCsv).toMatch(actual)
+      })
+
+      const tempFile = tmp.fileSync({ postfix: '.zip', keep: true })
+      const output = tempFile.name
+      const outputStream = fs.createWriteStream(output)
+
+      // Extract data from zip file and test
+      outputStream.on('finish', () => {
+        fs
+          .createReadStream(output)
+          .pipe(unzip.Parse())
+          .on('entry', entry => {
+            if (entry.path === 'products/product-type-1.csv')
+              entry.pipe(verifyCsv1)
+            else if (entry.path === 'products/product-type-2.csv')
+              entry.pipe(verifyCsv2)
+          })
+          .on('finish', () => {
+            tempFile.removeCallback()
+            done()
+          })
+      })
+
+      writer.writeToZipFile(sampleStream, outputStream, logger)
+    })
+
+    it('log success info on completion', done => {
+      const sampleStream = highland(sampleProducts)
+
+      const tempFile = tmp.fileSync({ postfix: '.zip' })
+      const output = tempFile.name
+      const outputStream = fs.createWriteStream(output)
+
+      outputStream.on('finish', () => {
+        expect(logger.info).toBeCalledWith(
+          expect.stringMatching(/written to ZIP file/)
+        )
+        tempFile.removeCallback()
+        done()
+      })
+
+      writer.writeToZipFile(sampleStream, outputStream, logger)
+    })
+  })
+})
