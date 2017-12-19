f193f5aec6a56ab89939ac6ecffbd45dd13971b0
tdeekens <nerd@tdeekens.name>
2017-12-19 13:13:20 +0100

    feat(sync-actions): add custom fields to cart discount sync

--- packages/sync-actions/src/cart-discounts.js
+++ packages/sync-actions/src/cart-discounts.js
@@ -1,10 +1,11 @@
 import flatten from 'lodash.flatten'
 import createBuildActions from './utils/create-build-actions'
 import createMapActionGroup from './utils/create-map-action-group'
+import actionsMapCustom from './utils/create-map-action-custom'
 import { actionsMapBase } from './cart-discounts-actions'
 import * as diffpatcher from './utils/diffpatcher'
 
-export const actionGroups = ['base']
+export const actionGroups = ['base', 'custom']
 
 function createCartDiscountsMapActions(mapActionGroup) {
   return function doMapActions(diff, newObj, oldObj) {
@@ -12,6 +13,10 @@ function createCartDiscountsMapActions(mapActionGroup) {
 
     allActions.push(
       mapActionGroup('base', () => actionsMapBase(diff, oldObj, newObj))
+    )
+
+    allActions.push(
+      mapActionGroup('custom', () => actionsMapCustom(diff, oldObj, newObj))
     )
 
     return flatten(allActions)
