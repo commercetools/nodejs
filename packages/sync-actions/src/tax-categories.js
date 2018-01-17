f6bdd88b25db131c5f59ad2cb35959d0c8fbe9be
Daniel Martínez <damarna85@gmail.com>
2018-01-17 08:54:26 +0000

    feat(sync-actions): add sync actions for tax categories (#406)
    affects: @commercetools/sync-actions
    
    add sync actions for tax categories

---
+++ packages/sync-actions/src/tax-categories.js
@@ -0,0 +1,47 @@
+/* @flow */
+import flatten from 'lodash.flatten'
+import type { SyncAction, ActionGroup } from '../../../types/sdk'
+import createBuildActions from './utils/create-build-actions'
+import createMapActionGroup from './utils/create-map-action-group'
+import * as taxCategoriesActions from './tax-categories-actions'
+import * as diffpatcher from './utils/diffpatcher'
+
+export const actionGroups = ['base', 'rates']
+
+function createTaxCategoriesMapActions(mapActionGroup) {
+  return function doMapActions(diff, newObj, oldObj /* , options */) {
+    const allActions = []
+
+    allActions.push(
+      mapActionGroup('base', () =>
+        taxCategoriesActions.actionsMapBase(diff, oldObj, newObj)
+      )
+    )
+
+    allActions.push(
+      mapActionGroup('rates', () =>
+        taxCategoriesActions.actionsMapRates(diff, oldObj, newObj)
+      )
+    )
+
+    return flatten(allActions)
+  }
+}
+
+export default (config: Array<ActionGroup>): SyncAction => {
+  // config contains information about which action groups
+  // are white/black listed
+
+  // createMapActionGroup returns function 'mapActionGroup' that takes params:
+  // - action group name
+  // - callback function that should return a list of actions that correspond
+  //    to the for the action group
+
+  // this resulting function mapActionGroup will call the callback function
+  // for whitelisted action groups and return the return value of the callback
+  // It will return an empty array for blacklisted action groups
+  const mapActionGroup = createMapActionGroup(config)
+  const doMapActions = createTaxCategoriesMapActions(mapActionGroup)
+  const buildActions = createBuildActions(diffpatcher.diff, doMapActions)
+  return { buildActions }
+}
