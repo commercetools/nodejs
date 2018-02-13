225a55a8b43992457fc05fc39d669bbb9d05d514
tdeekens <nerd@tdeekens.name>
2018-02-13 13:24:36 +0100

    chore(api-request-builder): add update, del and create as implicit featuresD

--- packages/api-request-builder/src/features.js
+++ packages/api-request-builder/src/features.js
@@ -1,3 +1,23 @@
+/**
+ * NOTE:
+ *  These are so called implicit features
+ *  which work on endpoints automatically as they only
+ *  differ in the http method and request body
+ *  and do not need any further processing.
+ *  We specify these on endpoints for documentation purposes
+ *  only.
+ */
+export const create = 'create'
+export const update = 'update'
+// `delete` is a reserved word in JavaScript
+export const del = 'delete'
+
+/**
+ * NOTE:
+ *  These are so called explicit features
+ *  which only work on a subset of endpoints and perform
+ *  additional manipulation on the request.
+ */
 export const query = 'query'
 export const queryOne = 'queryOne'
 export const queryExpand = 'queryExpand'
