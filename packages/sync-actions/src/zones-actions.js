56a1812997d2227768eaf99b9ee10c4dfa31aa50
qmateub <qmateub@yahoo.es>
2018-03-01 12:59:11 +0100

    fix(zones-locations): fix zone locations updates
    affects: @commercetools/sync-actions
    
    fix zone locations updates

--- packages/sync-actions/src/zones-actions.js
+++ packages/sync-actions/src/zones-actions.js
@@ -25,20 +25,45 @@ export function actionsMapLocations(diff, oldObj, newObj) {
       action: 'addLocation',
       location: newLocation,
     }),
-    [REMOVE_ACTIONS]: oldLocation => ({
-      action: 'removeLocation',
-      location: oldLocation,
-    }),
-    [CHANGE_ACTIONS]: (oldLocation, newLocation) => [
-      {
-        action: 'removeLocation',
-        location: oldLocation,
-      },
-      {
-        action: 'addLocation',
-        location: newLocation,
-      },
-    ],
+    [REMOVE_ACTIONS]: oldLocation =>
+      // We only add the action if the location is not included in the new object.
+      !newObj.locations.find(
+        location => location.country === oldLocation.country
+      )
+        ? {
+            action: 'removeLocation',
+            location: oldLocation,
+          }
+        : null,
+    [CHANGE_ACTIONS]: (oldLocation, newLocation) => {
+      const result = []
+
+      // We only remove the location in case that the oldLocation is not
+      // included in the new object
+      if (
+        !newObj.locations.find(
+          location => location.country === oldLocation.country
+        )
+      )
+        result.push({
+          action: 'removeLocation',
+          location: oldLocation,
+        })
+
+      // We only add the location in case that the newLocation was not
+      // included in the old object
+      if (
+        !oldObj.locations.find(
+          location => location.country === newLocation.country
+        )
+      )
+        result.push({
+          action: 'addLocation',
+          location: newLocation,
+        })
+
+      return result
+    },
   })
 
   return handler(diff, oldObj, newObj)
