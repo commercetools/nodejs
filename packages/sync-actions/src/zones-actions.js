a1801415acfc9cb1c63ef65fd7ce468f835bd0b3
qmateub <qmateub@yahoo.es>
2018-03-01 14:17:32 +0100

    refactor(util): extract shared code to a util function to checking for existing locations
    affects: @commercetools/sync-actions
    
    extract shared code to a util function to checking for existing locations

--- packages/sync-actions/src/zones-actions.js
+++ packages/sync-actions/src/zones-actions.js
@@ -10,6 +10,9 @@ export const baseActionsList = [
   { action: 'setDescription', key: 'description' },
 ]
 
+const hasLocation = (locations, otherLocation) =>
+  locations.some(location => location.country === otherLocation.country)
+
 export function actionsMapBase(diff, oldObj, newObj) {
   return buildBaseAttributesActions({
     actions: baseActionsList,
@@ -27,9 +30,7 @@ export function actionsMapLocations(diff, oldObj, newObj) {
     }),
     [REMOVE_ACTIONS]: oldLocation =>
       // We only add the action if the location is not included in the new object.
-      !newObj.locations.find(
-        location => location.country === oldLocation.country
-      )
+      !hasLocation(newObj.locations, oldLocation)
         ? {
             action: 'removeLocation',
             location: oldLocation,
@@ -40,11 +41,7 @@ export function actionsMapLocations(diff, oldObj, newObj) {
 
       // We only remove the location in case that the oldLocation is not
       // included in the new object
-      if (
-        !newObj.locations.find(
-          location => location.country === oldLocation.country
-        )
-      )
+      if (!hasLocation(newObj.locations, oldLocation))
         result.push({
           action: 'removeLocation',
           location: oldLocation,
@@ -52,11 +49,7 @@ export function actionsMapLocations(diff, oldObj, newObj) {
 
       // We only add the location in case that the newLocation was not
       // included in the old object
-      if (
-        !oldObj.locations.find(
-          location => location.country === newLocation.country
-        )
-      )
+      if (!hasLocation(oldObj.locations, newLocation))
         result.push({
           action: 'addLocation',
           location: newLocation,
