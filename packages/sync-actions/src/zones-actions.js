e95d64f81c3d31babee67bf7f6e1e3778252c2f6
Quique Mateu <qmateub@yahoo.es>
2018-03-01 17:05:20 +0100

    fix(zones-locations): fix zone locations updates (#482)
    * fix(zones-locations): fix zone locations updates
    
    affects: @commercetools/sync-actions
    
    fix zone locations updates
    
    * refactor(util): extract shared code to a util function to checking for existing locations
    
    affects: @commercetools/sync-actions
    
    extract shared code to a util function to checking for existing locations

--- packages/sync-actions/src/zones-actions.js
+++ packages/sync-actions/src/zones-actions.js
@@ -10,6 +10,9 @@ export const baseActionsList = [
   { action: 'setDescription', key: 'description' },
 ]
 
+const hasLocation = (locations, otherLocation) =>
+  locations.some(location => location.country === otherLocation.country)
+
 export function actionsMapBase(diff, oldObj, newObj) {
   return buildBaseAttributesActions({
     actions: baseActionsList,
@@ -25,20 +28,35 @@ export function actionsMapLocations(diff, oldObj, newObj) {
       action: 'addLocation',
       location: newLocation,
     }),
-    [REMOVE_ACTIONS]: oldLocation => ({
-      action: 'removeLocation',
-      location: oldLocation,
-    }),
-    [CHANGE_ACTIONS]: (oldLocation, newLocation) => [
-      {
-        action: 'removeLocation',
-        location: oldLocation,
-      },
-      {
-        action: 'addLocation',
-        location: newLocation,
-      },
-    ],
+    [REMOVE_ACTIONS]: oldLocation =>
+      // We only add the action if the location is not included in the new object.
+      !hasLocation(newObj.locations, oldLocation)
+        ? {
+            action: 'removeLocation',
+            location: oldLocation,
+          }
+        : null,
+    [CHANGE_ACTIONS]: (oldLocation, newLocation) => {
+      const result = []
+
+      // We only remove the location in case that the oldLocation is not
+      // included in the new object
+      if (!hasLocation(newObj.locations, oldLocation))
+        result.push({
+          action: 'removeLocation',
+          location: oldLocation,
+        })
+
+      // We only add the location in case that the newLocation was not
+      // included in the old object
+      if (!hasLocation(oldObj.locations, newLocation))
+        result.push({
+          action: 'addLocation',
+          location: newLocation,
+        })
+
+      return result
+    },
   })
 
   return handler(diff, oldObj, newObj)
