84879c62b0645bee6da00acca7a3c42246af092c
daern91 <daniel_eriksson_8@hotmail.com>
2018-03-19 17:32:11 +0100

    feat(sync-actions): support price selection

--- packages/sync-actions/src/utils/create-build-actions.js
+++ packages/sync-actions/src/utils/create-build-actions.js
@@ -1,5 +1,51 @@
+import isEqual from 'lodash.isequal'
+
 function applyOnBeforeDiff(before, now, fn) {
   return fn && typeof fn === 'function' ? fn(before, now) : [before, now]
+}
+
+function addPriceId(newPrice, beforeVariantArray) {
+  let getId = ''
+  const comparisonNewPrice = {
+    value: { currencyCode: newPrice.value.currencyCode },
+    channel: newPrice.channel,
+    country: newPrice.country,
+    customerGroup: newPrice.customerGroup,
+  }
+
+  beforeVariantArray.map(variant =>
+    variant.prices.find(oldPrice => {
+      const { value, channel, country, customerGroup } = oldPrice
+
+      const comparisonOldPrice = {
+        value: { currencyCode: value.currencyCode },
+        channel,
+        country,
+        customerGroup,
+      }
+      if (isEqual(comparisonNewPrice, comparisonOldPrice)) {
+        getId = oldPrice.id
+        return true
+      }
+      return false
+    })
+  )
+
+  return getId
+}
+
+function isThereAMissingPriceId(nowVariantArray, beforeVariantArray) {
+  nowVariantArray.map(variant => {
+    if (!variant.prices) return variant
+    return variant.prices.map(price => {
+      const priceWithId = price
+      if (!priceWithId.id) {
+        const id = addPriceId(price, beforeVariantArray)
+        if (id) priceWithId.id = id
+      }
+      return priceWithId
+    })
+  })
 }
 
 export default function createBuildActions(differ, doMapActions, onBeforeDiff) {
@@ -15,6 +61,9 @@ export default function createBuildActions(differ, doMapActions, onBeforeDiff) {
       now,
       onBeforeDiff
     )
+
+    if (processedNow.variants)
+      isThereAMissingPriceId(processedNow.variants, processedBefore.variants)
 
     const diffed = differ(processedBefore, processedNow)
 
--- packages/sync-actions/test/product-sync-prices.spec.js
+++ packages/sync-actions/test/product-sync-prices.spec.js
@@ -167,4 +167,119 @@ describe('Actions', () => {
     const actions = productsSync.buildActions(now, before)
     expect(actions).toEqual([])
   })
+
+  describe('build actions for scope without ID', () => {
+    const shuffleArray = array =>
+      array.reduce(
+        (agg, val) =>
+          agg.splice(Math.floor(Math.random() * agg.length), 0, val) && agg,
+        []
+      )
+
+    const before = {
+      id: '123-abc',
+      masterVariant: {
+        id: 1,
+        prices: [
+          {
+            id: '111',
+            value: { currencyCode: 'EUR', centAmount: 3000 },
+            country: 'US',
+            customerGroup: { typeId: 'customer-group', id: 'cg1' },
+            channel: { typeId: 'channel', id: 'ch1' },
+          },
+          {
+            id: '222',
+            value: { currencyCode: 'USD', centAmount: 5000 },
+            country: 'US',
+            customerGroup: { typeId: 'customer-group', id: 'cg1' },
+          },
+          {
+            id: '333',
+            value: { currencyCode: 'SEK', centAmount: 10000 },
+            country: 'US',
+            channel: { typeId: 'channel', id: 'ch1' },
+          },
+        ],
+      },
+    }
+    const pricesWithoutId = [
+      {
+        value: { currencyCode: 'EUR', centAmount: 4000 },
+        country: 'US',
+        customerGroup: { typeId: 'customer-group', id: 'cg1' },
+        channel: { typeId: 'channel', id: 'ch1' },
+      },
+      {
+        value: { currencyCode: 'USD', centAmount: 6000 },
+        country: 'US',
+        customerGroup: { typeId: 'customer-group', id: 'cg1' },
+      },
+      {
+        value: { currencyCode: 'SEK', centAmount: 15000 },
+        country: 'US',
+        channel: { typeId: 'channel', id: 'ch1' },
+      },
+      {
+        value: { currencyCode: 'EUR', centAmount: 1000 },
+        country: 'DE',
+      },
+    ]
+    const now = {
+      id: '456-def',
+      masterVariant: {
+        id: 1,
+        prices: shuffleArray(pricesWithoutId),
+      },
+    }
+
+    it('should build actions even if id is not supplied', () => {
+      const actions = productsSync.buildActions(now, before)
+
+      expect(actions).toEqual(
+        expect.arrayContaining([
+          {
+            action: 'changePrice',
+            priceId: '111',
+            price: {
+              id: '111',
+              value: { currencyCode: 'EUR', centAmount: 4000 },
+              country: 'US',
+              customerGroup: { typeId: 'customer-group', id: 'cg1' },
+              channel: { typeId: 'channel', id: 'ch1' },
+            },
+          },
+          {
+            action: 'changePrice',
+            priceId: '222',
+            price: {
+              id: '222',
+              value: { currencyCode: 'USD', centAmount: 6000 },
+              country: 'US',
+              customerGroup: { typeId: 'customer-group', id: 'cg1' },
+            },
+          },
+          {
+            action: 'changePrice',
+            priceId: '333',
+            price: {
+              id: '333',
+              value: { currencyCode: 'SEK', centAmount: 15000 },
+              country: 'US',
+
+              channel: { typeId: 'channel', id: 'ch1' },
+            },
+          },
+          {
+            action: 'addPrice',
+            price: {
+              country: 'DE',
+              value: { centAmount: 1000, currencyCode: 'EUR' },
+            },
+            variantId: 1,
+          },
+        ])
+      )
+    })
+  })
 })
