aa77717dbd1ff21b18e2bfce2e2d44266aae5503
Williams Omayuku <wizzy25@users.noreply.github.com>
2018-03-19 12:05:27 +0100

    feat(sync-actions): add state update actions (#495)
    * feat(sync-actions): add state update actions
    
    affects: @commercetools/sync-actions
    
    Add support for update actions supported by state
    
    * refactor(state-sync): remove redundant "change-actions" method
    
    affects: @commercetools/sync-actions
    
    * docs(states-sync): add documentation for states sync actions
    
    affects: @commercetools/sync-actions
    
    * refactor(state-sync): minor refactor
    
    affects: @commercetools/sync-actions
    
    rename 'handler' function to 'buildArrayActions'
    add role update action flowtype

---
+++ packages/sync-actions/src/state-actions.js
@@ -0,0 +1,38 @@
+import { buildBaseAttributesActions } from './utils/common-actions'
+import createBuildArrayActions, {
+  ADD_ACTIONS,
+  REMOVE_ACTIONS,
+} from './utils/create-build-array-actions'
+
+export const baseActionsList = [
+  { action: 'changeKey', key: 'key' },
+  { action: 'setName', key: 'name' },
+  { action: 'setDescription', key: 'description' },
+  { action: 'changeType', key: 'type' },
+  { action: 'changeInitial', key: 'initial' },
+  { action: 'setTransitions', key: 'transitions' },
+]
+
+export function actionsMapBase(diff, oldObj, newObj) {
+  return buildBaseAttributesActions({
+    actions: baseActionsList,
+    diff,
+    oldObj,
+    newObj,
+  })
+}
+
+export function actionsMapRoles(diff, oldObj, newObj) {
+  const buildArrayActions = createBuildArrayActions('roles', {
+    [ADD_ACTIONS]: newRole => ({
+      action: 'addRoles',
+      roles: newRole,
+    }),
+    [REMOVE_ACTIONS]: oldRole => ({
+      action: 'removeRoles',
+      roles: oldRole,
+    }),
+  })
+
+  return buildArrayActions(diff, oldObj, newObj)
+}
