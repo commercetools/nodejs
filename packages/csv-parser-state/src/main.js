239ba8ede238c7af818c2409c7f02ab9c66e14ad
wizzy25 <womayuku@gmail.com>
2018-03-27 16:04:01 +0200

    feat(state-parser): implement functions to transform states
    affects: @commercetools/csv-parser-state

--- packages/csv-parser-state/src/main.js
+++ packages/csv-parser-state/src/main.js
@@ -1,3 +1,86 @@
-export default function add(a, b) {
-  return a + b
+import { createClient } from '@commercetools/sdk-client'
+import { createRequestBuilder } from '@commercetools/api-request-builder'
+import { createHttpMiddleware } from '@commercetools/sdk-middleware-http'
+import { createAuthMiddlewareForClientCredentialsFlow } from '@commercetools/sdk-middleware-auth'
+import { createUserAgentMiddleware } from '@commercetools/sdk-middleware-user-agent'
+import pkg from '../package.json'
+
+export default class CsvParserState {
+  constructor({ apiConfig = null, csvConfig = {} }, logger) {
+    this.apiConfig = apiConfig
+
+    const defaultOptions = {
+      delimiter: ',',
+      multiValueDelimiter: ';',
+      continueOnProblems: false,
+    }
+    this.csvConfig = { ...defaultOptions, ...csvConfig }
+    this.logger = logger || {
+      error: () => {},
+      warn: () => {},
+      info: () => {},
+      debug: () => {},
+    }
+  }
+
+  async _transformTransitions({ transitions, ...restOfState }) {
+    if (!transitions) return restOfState
+    // We setup the client here because it is unnecessary
+    // if there are no transitions
+    this._setupClient()
+    const stateRequests = transitions.map(transitionState =>
+      this._buildStateRequest(transitionState)
+    )
+    const resolvedStates = await Promise.all(stateRequests)
+    const stateReferences = resolvedStates.map(response => ({
+      typeId: 'state',
+      id: response.body.id,
+    }))
+    return { ...restOfState, transitions: stateReferences }
+  }
+
+  _buildStateRequest(stateKey) {
+    const stateService = this._createStateService()
+    const uri = stateService.byKey(stateKey).build()
+    return this.client.execute({ uri, method: 'GET' })
+  }
+
+  _setupClient() {
+    if (!this.apiConfig)
+      throw new Error('The constructor must be passed an `apiConfig` object')
+    this.client = createClient({
+      middlewares: [
+        createAuthMiddlewareForClientCredentialsFlow(this.apiConfig),
+        createUserAgentMiddleware({
+          libraryName: pkg.name,
+          libraryVersion: pkg.version,
+        }),
+        createHttpMiddleware({ host: this.apiConfig.apiUrl }),
+      ],
+    })
+  }
+
+  _createStateService() {
+    return createRequestBuilder({
+      projectKey: this.apiConfig.projectKey,
+    }).states
+  }
+
+  _mapTransitionsToArray({ transitions, ...restOfState }) {
+    return transitions
+      ? {
+          ...restOfState,
+          transitions: transitions.split(this.csvConfig.multiValueDelimiter),
+        }
+      : restOfState
+  }
+
+  // Remove fields with empty values from the state objects
+  static _removeEmptyFields(item) {
+    // "acc" is "state" but lint doesn't like mutations
+    return Object.entries(item).reduce((acc, entry) => {
+      if (entry[1] !== '') acc[entry[0]] = entry[1]
+      return acc
+    }, {})
+  }
 }
--- packages/csv-parser-state/test/main.spec.js
+++ packages/csv-parser-state/test/main.spec.js
@@ -1,7 +1,251 @@
-import add from '../src/main'
+import fs from 'fs'
+import path from 'path'
+import streamtest from 'streamtest'
+import CsvParserState from '../src/main'
 
-describe('::add', () => {
-  test('should add two numbers', () => {
-    expect(add(3, 5)).toBe(8)
+describe('CsvParserState', () => {
+  const logger = {
+    error: () => {},
+    warn: () => {},
+    info: () => {},
+    debug: () => {},
+  }
+
+  let csvParser
+  beforeEach(() => {
+    csvParser = new CsvParserState(
+      {
+        apiConfig: {
+          projectKey: 'myProjectKey',
+        },
+        accessToken: 'myAccessToken',
+      },
+      logger
+    )
+  })
+
+  describe('::constructor', () => {
+    test('should set default properties', () => {
+      expect(csvParser.csvConfig.delimiter).toBe(',')
+      expect(csvParser.csvConfig.multiValueDelimiter).toBe(';')
+      expect(csvParser.csvConfig.continueOnProblems).toBeFalsy()
+    })
+  })
+
+  xdescribe('::parse', () => {
+    test('should successfully parse CSV to JSON', done => {
+      const inputStream = fs.createReadStream(
+        path.join(__dirname, 'helpers/sampleStates.csv')
+      )
+
+      const outputStream = streamtest.v2.toText((err, data) => {
+        const result = JSON.parse(data)
+        expect(result).toBeInstanceOf(Array)
+        expect(result).toHaveLength(4)
+        console.log(data)
+        done()
+      })
+      csvParser.parse(inputStream, outputStream)
+    })
+  })
+
+  describe('_transformTransitions', () => {
+    describe('without transitions', () => {
+      test('should not mutate state', async () => {
+        const actual = {
+          foo: 'bar',
+          key: 'my-state',
+        }
+        expect(csvParser._transformTransitions(actual)).resolves.toEqual(actual)
+      })
+    })
+
+    describe('with transitions', () => {
+      let setupClientSpy
+      const state = { key: 'state-key', transitions: ['state-1', 'state-2'] }
+      beforeEach(() => {
+        setupClientSpy = jest.spyOn(csvParser, '_setupClient')
+        csvParser._buildStateRequest = jest.fn(() =>
+          Promise.resolve({
+            body: {
+              results: [
+                {
+                  id: 'state-id-1',
+                  key: 'state-key-1',
+                },
+              ],
+            },
+          })
+        )
+      })
+
+      afterEach(() => {
+        setupClientSpy.mockReset()
+        setupClientSpy.mockRestore()
+      })
+
+      test('should setup client', () => {
+        csvParser._transformTransitions(state)
+        expect(setupClientSpy).toBeCalled()
+      })
+
+      describe('when execute resolves', () => {
+        beforeEach(() => {
+          // First transition exists but second does not
+          csvParser._buildStateRequest = jest
+            .fn()
+            .mockImplementationOnce(() =>
+              Promise.resolve({
+                body: {
+                  id: 'state-id-1',
+                  key: 'state-1',
+                },
+              })
+            )
+            .mockImplementation(() =>
+              Promise.resolve({
+                body: {
+                  id: 'state-id-2',
+                  key: 'state-2',
+                },
+              })
+            )
+        })
+
+        test('should resolve with modified state', () => {
+          expect(csvParser._transformTransitions(state)).resolves.toEqual({
+            key: 'state-key',
+            transitions: [
+              {
+                id: 'state-id-1',
+                typeId: 'state',
+              },
+              {
+                id: 'state-id-2',
+                typeId: 'state',
+              },
+            ],
+          })
+        })
+      })
+
+      describe('when execute rejects', () => {
+        beforeEach(() => {
+          csvParser._buildStateRequest = jest.fn(() =>
+            Promise.reject(new Error('Invalid Request'))
+          )
+        })
+
+        test('should reject with execute rejection error', () => {
+          expect(csvParser._transformTransitions(state)).rejects.toThrow(
+            /Invalid Request/
+          )
+        })
+      })
+    })
+  })
+
+  describe('_buildStateRequest', () => {
+    beforeEach(() => {
+      csvParser.client = { execute: jest.fn() }
+    })
+
+    test('should create a state service', done => {
+      // We also ensure the `.build()` method is called
+      csvParser._createStateService = jest.fn(() => ({
+        byKey: () => ({
+          build: () => done(),
+        }),
+      }))
+      csvParser._buildStateRequest('state-key')
+      expect(csvParser._createStateService).toBeCalled()
+      expect(csvParser.client.execute).toBeCalledWith(
+        expect.objectContaining({ method: 'GET' })
+      )
+    })
+  })
+
+  describe('_setupClient', () => {
+    describe('without `apiConfig`', () => {
+      beforeEach(() => {
+        csvParser = new CsvParserState({ foo: 'bar' }, logger)
+      })
+
+      test('should throw error', () => {
+        expect(() => csvParser._setupClient()).toThrowError(
+          /The constructor must be passed an `apiConfig` object/
+        )
+      })
+    })
+
+    describe('with `apiConfig`', () => {
+      test('should build client', () => {
+        // Before executing the function
+        expect(csvParser.client).not.toBeDefined()
+        // After executing the function
+        csvParser._setupClient()
+        expect(csvParser.client).toBeDefined()
+        expect(csvParser.client).toEqual(
+          expect.objectContaining({
+            execute: expect.any(Function),
+            process: expect.any(Function),
+          })
+        )
+      })
+    })
+  })
+
+  describe('::_createStateService', () => {
+    test('should be defined', () => {
+      expect(csvParser._createStateService).toBeDefined()
+      expect(csvParser._createStateService()).toEqual(
+        expect.objectContaining({
+          byKey: expect.any(Function),
+          build: expect.any(Function),
+          parse: expect.any(Function),
+        })
+      )
+    })
+  })
+
+  describe('::_mapTransitionsToArray', () => {
+    describe('with transitions', () => {
+      test('should convert `transitions` property to an Array', () => {
+        const actual = {
+          foo: 'bar',
+          transitions: 'my-state-1;my-state-2',
+        }
+        const expected = {
+          foo: 'bar',
+          transitions: ['my-state-1', 'my-state-2'],
+        }
+        expect(csvParser._mapTransitionsToArray(actual)).toEqual(expected)
+      })
+    })
+
+    describe('without transitions', () => {
+      test('should not mutate state', () => {
+        const actual = {
+          foo: 'bar',
+          key: 'my-state',
+        }
+        expect(csvParser._mapTransitionsToArray(actual)).toEqual(actual)
+      })
+    })
+  })
+
+  describe('::_removeEmptyFields', () => {
+    test('should remove empty fields from state objects', () => {
+      const actual = {
+        foo: 'bar',
+        empty: '',
+        some: 'all',
+      }
+      const expected = {
+        foo: 'bar',
+        some: 'all',
+      }
+      expect(CsvParserState._removeEmptyFields(actual)).toEqual(expected)
+    })
   })
 })
