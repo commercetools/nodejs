ce5832680e705f94a6a693fe404c5424ccdbe8fb
tdeekens <tobias.deekens@commercetools.de>
2018-04-26 18:16:12 +0200

    chore: remove browser field in pkg

--- rollup.config.js
+++ rollup.config.js
@@ -24,8 +24,15 @@ const config = {
       VERSION: `'${version}'`,
     }),
     resolve({
+      /**
+       * NOTE:
+       *   The is a cascade performed by rollup-plugin-node-resolve. It first
+       *   attempts to find a browser, then module and then main build in the pkg.
+       *
+       *   https://github.com/rollup/rollup-plugin-node-resolve/blob/7846a19da76e84f81f349162e6cf281550f6a67d/src/index.js#L136-L144
+       */
       module: true,
-      jsnext: true,
+      browser: true,
       main: true,
       preferBuiltins: true,
     }),
