5684a86193022f4118577e4ad05f485153fe5ae9
Adnan Asani <adnasa@users.noreply.github.com>
2018-05-17 10:56:02 +0200

    fix(sync-actions/product-types): generate update actions for changes in enum values
    * fix(sync-actions/product-types): assume correct shape of attribute, with `enum` type
    
    * refactor(sync-actions): simplify actionName assignment
    
    * test(sync-actions/product-types): inline attribute-definitions

--- packages/sync-actions/src/product-types-actions.js
+++ packages/sync-actions/src/product-types-actions.js
@@ -50,21 +50,20 @@ const getIsAnAttributeDefinitionBaseFieldChange = diff =>
   diff.isSearchable
 
 function actionsMapEnums(attributeType, attributeDiff, previous, next) {
-  const getAddActionName = type =>
-    type === 'enum' ? 'addPlainEnumValue' : 'addLocalizedEnumValue'
-  const getChangeEnumOrderActionName = type =>
-    type === 'enum'
+  const addEnumActionName =
+    attributeType === 'enum' ? 'addPlainEnumValue' : 'addLocalizedEnumValue'
+  const changeEnumOrderActionName =
+    attributeType === 'enum'
       ? 'changePlainEnumValueOrder'
       : 'changeLocalizedEnumValueOrder'
-  const getChangeEnumLabelActionName = type =>
-    type === 'enum'
+  const changeEnumLabelActionName =
+    attributeType === 'enum'
       ? 'changePlainEnumValueLabel'
       : 'changeLocalizedEnumValueLabel'
-
   const buildArrayActions = createBuildArrayActions('values', {
     [ADD_ACTIONS]: newEnum => ({
       attributeName: next.name,
-      action: getAddActionName(attributeType),
+      action: addEnumActionName,
       value: newEnum,
     }),
     [CHANGE_ACTIONS]: (oldEnum, newEnum) => {
@@ -81,13 +80,13 @@ function actionsMapEnums(attributeType, attributeDiff, previous, next) {
         if (!deepEqual(oldEnum.label, oldEnumInNext.label)) {
           changeActions.push({
             attributeName: next.name,
-            action: getChangeEnumLabelActionName(attributeType),
+            action: changeEnumLabelActionName,
             newValue: newEnum,
           })
         } else {
           changeActions.push({
             attributeName: next.name,
-            action: getChangeEnumOrderActionName(attributeType),
+            action: changeEnumOrderActionName,
             value: newEnum,
           })
         }
@@ -99,7 +98,7 @@ function actionsMapEnums(attributeType, attributeDiff, previous, next) {
         })
         changeActions.push({
           attributeName: next.name,
-          action: getAddActionName(attributeType),
+          action: addEnumActionName,
           value: newEnum,
         })
       }
@@ -122,9 +121,7 @@ function actionsMapEnums(attributeType, attributeDiff, previous, next) {
     updateAction => {
       if (updateAction.action === 'removeEnumValue')
         removedKeys.push(updateAction.value.key)
-      else if (
-        updateAction.action === getChangeEnumOrderActionName(attributeType)
-      ) {
+      else if (updateAction.action === changeEnumOrderActionName) {
         newEnumValuesOrder.push(updateAction.value)
       } else actions.push(updateAction)
     }
@@ -136,7 +133,7 @@ function actionsMapEnums(attributeType, attributeDiff, previous, next) {
       ? [
           {
             attributeName: next.name,
-            action: getChangeEnumOrderActionName(attributeType),
+            action: changeEnumOrderActionName,
             values: newEnumValuesOrder,
           },
         ]
@@ -167,6 +164,7 @@ export function actionsMapAttributes(
       previous,
       next
     )
+
     if (getIsChangedOperation(diffKey)) {
       if (Array.isArray(diffValue)) {
         const deltaValue = diffpatcher.getDeltaValue(diffValue)
@@ -188,13 +186,13 @@ export function actionsMapAttributes(
             attributeName: extractedPairs.oldObj.name,
           }))
         )
-      } else if (diffValue.values) {
+      } else if (diffValue.type.values) {
         actions.push(
           ...actionsMapEnums(
             extractedPairs.oldObj.type.name,
-            diffValue,
-            extractedPairs.oldObj,
-            extractedPairs.newObj
+            diffValue.type,
+            extractedPairs.oldObj.type,
+            extractedPairs.newObj.type
           )
         )
       }
