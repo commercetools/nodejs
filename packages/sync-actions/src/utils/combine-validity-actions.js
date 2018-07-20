8f205280a7978426dd5728098b3d257cf436ebc8
Islam ezzat <islam3zzat@gmail.com>
2018-07-20 16:26:44 +0200

    refactor(validity-actions): refactor action util

--- packages/sync-actions/src/utils/combine-validity-actions.js
+++ packages/sync-actions/src/utils/combine-validity-actions.js
@@ -1,13 +1,14 @@
+const validityActions = ['setValidFrom', 'setValidUntil']
+
+const isValidityActions = actionName => validityActions.includes(actionName)
+
 export default function combineValidityActions(actions = []) {
-  const [setValidFromAction, setValidUntilAction] = actions.filter(
-    item => item.action === 'setValidFrom' || item.action === 'setValidUntil'
+  const [setValidFromAction, setValidUntilAction] = actions.filter(item =>
+    isValidityActions(item.action)
   )
   if (setValidFromAction && setValidUntilAction) {
     return [
-      ...actions.filter(
-        item =>
-          !(item.action === 'setValidFrom' || item.action === 'setValidUntil')
-      ),
+      ...actions.filter(item => !isValidityActions(item.action)),
       {
         action: 'setValidFromAndUntil',
         validFrom: setValidFromAction.validFrom,
