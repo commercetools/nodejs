bc6a3eee1d891317ffe2f9675309f0d07ee5bcd7
Daniel Eriksson <daniel_eriksson_8@hotmail.com>
2018-09-11 11:22:51 +0200

    feat(babel): migrate to babel@7 (#790)
    #### Summary
    
    PR migrates from **babel@6** to **@7**. 
    Big thanks for the pointers and inspiration from @tdeekens's PR that did the same here: https://github.com/tdeekens/flopflip/pull/465
    
    #### Description
    
    One thing to notice here is the solution on how to use the config file. Babel 7 removes support for `.babelrc` inside of monorepos ([ref](https://babeljs.io/docs/en/config-files#6x-vs-7x-babelrc-loading)).
    
    We will now use `babel.config.js` instead ([ref](https://babeljs.io/docs/en/config-files#project-wide-configuration)). However, please see the following quote:
    
    > The primary downside of this project-wide config is that, because it relies on the working directory, it can be more painful to use in monorepos if the working directory is not the monorepo root. For example, if you have
    > ```
    >babel.config.js
    >packages/
    >  mod1/
    >    package.json
    >    src/index.js
    >  mod2/
    >    package.json
    >    src/index.js
    > ```
    > and the individual packages are responsible for running their builds (and their working directory for Babel is the individual packages), the babel.config.js file will not be automatically loaded, and users will be required to set the path to it manually.
    
    So, PR also changes the `build script` across all packages. This might not be the ideal solution so I'd love to get any thoughts and input on it. Would be great if we could find another way.
    
    resolves #768 
    resolves #773 
    resolves #763
    resolves #766

---
+++ babel.config.js
@@ -0,0 +1,47 @@
+const getPresets = () => {
+  switch (process.env.NODE_ENV) {
+    case 'development':
+      return {
+        targets: { node: 'current' },
+        modules: 'commonjs',
+        useBuiltIns: 'usage',
+        include: ['es7.object.entries'],
+      }
+    case 'rollup':
+      return {
+        targets: {
+          browsers: ['last 2 versions'],
+          node: 'current',
+        },
+        modules: false,
+      }
+    case 'production':
+      return {
+        targets: {
+          browsers: ['last 2 versions'],
+          node: '6',
+        },
+        modules: false,
+        useBuiltIns: 'usage',
+        include: ['es7.object.entries'],
+      }
+    case 'cli':
+      return {
+        targets: { node: '6' },
+        modules: 'commonjs',
+        useBuiltIns: 'usage',
+        include: ['es7.object.entries'],
+      }
+    default:
+      return {}
+  }
+}
+
+module.exports = {
+  presets: [['@babel/preset-env', getPresets()], '@babel/preset-flow'],
+  plugins: [
+    '@babel/plugin-proposal-export-default-from',
+    '@babel/plugin-proposal-export-namespace-from',
+    '@babel/plugin-proposal-object-rest-spread',
+  ],
+}
