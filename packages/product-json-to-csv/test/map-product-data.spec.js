72d80368ad52d3957e836584ce328d5ba36a4fee
Jan Jůna <jan.juna-ext@commercetools.de>
2018-10-17 11:07:07 +0200

    test(product-json-to-csv): improve test coverage

--- packages/product-json-to-csv/test/map-product-data.spec.js
+++ packages/product-json-to-csv/test/map-product-data.spec.js
@@ -643,6 +643,10 @@ describe('::ProductMapping', () => {
               name: 'normalAttribute',
               value: 'normalAttrValue',
             },
+            {
+              name: 'booleanAttribute',
+              value: true,
+            },
           ],
         },
         variants: [],
