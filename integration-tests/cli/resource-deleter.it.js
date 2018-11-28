6def2f79e8ea8179285080b4519ef07c29f64329
babanila <babanila@yahoo.com>
2018-11-28 15:35:24 +0100

    fix(resource-deleter): add test for each resource

--- integration-tests/cli/resource-deleter.it.js
+++ integration-tests/cli/resource-deleter.it.js
@@ -4,11 +4,24 @@ import { getCredentials } from '@commercetools/get-credentials'
 import { exec } from 'mz/child_process'
 import { version } from '@commercetools/resource-deleter/package.json'
 import {
-  cart,
+  carts,
   categories,
-  customer,
-  sampleProductType,
-  anotherSampleProductType,
+  channels,
+  customerGroups,
+  customers,
+  customObjects,
+  discountCodes,
+  inventoryEntries,
+  payments,
+  productDiscounts,
+  products,
+  productTypes,
+  reviews,
+  shippingMethods,
+  states,
+  taxCategories,
+  types,
+  zones,
 } from './helpers/resource-delete.data'
 import { createData, clearData } from './helpers/utils'
 
@@ -30,26 +43,52 @@ describe('Resource Deleter', () => {
       credentials,
     }
 
-    await clearData(apiConfig, 'carts')
-    await clearData(apiConfig, 'categories')
-    await clearData(apiConfig, 'customers')
-    await clearData(apiConfig, 'productTypes')
-
     // Create resources on API
-    await createData(apiConfig, 'carts', cart)
-    await createData(apiConfig, 'categories', categories)
-    await createData(apiConfig, 'customers', customer)
-    await createData(apiConfig, 'productTypes', [
-      sampleProductType,
-      anotherSampleProductType,
+    await Promise.all([
+      await Promise.all([
+        createData(apiConfig, 'productTypes', productTypes),
+        createData(apiConfig, 'categories', categories),
+        createData(apiConfig, 'taxCategories', taxCategories),
+        createData(apiConfig, 'channels', channels),
+        createData(apiConfig, 'customers', customers),
+        createData(apiConfig, 'inventory', inventoryEntries),
+        createData(apiConfig, 'customObjects', customObjects),
+        createData(apiConfig, 'discountCodes', discountCodes),
+        createData(apiConfig, 'payments', payments),
+        createData(apiConfig, 'customerGroups', customerGroups),
+        createData(apiConfig, 'reviews', reviews),
+        createData(apiConfig, 'productDiscounts', productDiscounts),
+        createData(apiConfig, 'zones', zones),
+        createData(apiConfig, 'types', types),
+      ]),
+      createData(apiConfig, 'products', products),
+      createData(apiConfig, 'shippingMethods', shippingMethods),
+      createData(apiConfig, 'states', states),
+      createData(apiConfig, 'carts', carts),
     ])
   }, 30000)
 
   afterAll(async () => {
-    await clearData(apiConfig, 'carts')
-    await clearData(apiConfig, 'categories')
-    await clearData(apiConfig, 'customers')
-    await clearData(apiConfig, 'productTypes')
+    await Promise.all([
+      clearData(apiConfig, 'productDiscounts'),
+      clearData(apiConfig, 'reviews'),
+      clearData(apiConfig, 'shippingMethods'),
+      clearData(apiConfig, 'states'),
+      clearData(apiConfig, 'taxCategories'),
+      clearData(apiConfig, 'types'),
+      clearData(apiConfig, 'zones'),
+      clearData(apiConfig, 'productTypes'),
+      clearData(apiConfig, 'categories'),
+      clearData(apiConfig, 'carts'),
+      clearData(apiConfig, 'products'),
+      clearData(apiConfig, 'channels'),
+      clearData(apiConfig, 'customerGroups'),
+      clearData(apiConfig, 'customers'),
+      clearData(apiConfig, 'customObjects'),
+      clearData(apiConfig, 'discountCodes'),
+      clearData(apiConfig, 'inventory'),
+      clearData(apiConfig, 'payments'),
+    ])
   })
 
   describe('CLI basic functionality', () => {
@@ -66,41 +105,523 @@ describe('Resource Deleter', () => {
     })
   })
 
-  describe('resource-deleter function', () => {
+  describe('when deleting carts', () => {
     let filePath
     let stdout
     let stderr
-    const resources = ['carts', 'categories', 'customers', 'productTypes']
-    const results = []
+    const resource = 'carts'
 
-    beforeEach(async () => {
+    beforeAll(async () => {
       filePath = tmp.fileSync().name
-
-      await Promise.all(
-        resources.map(async resource => {
-          ;[stdout, stderr] = await exec(
-            `${bin} -o ${filePath} -p ${projectKey} -r ${resource}`
-          )
-          results.push({ stdout, stderr })
-        })
+      ;[stdout, stderr] = await exec(
+        `${bin} -o ${filePath} -p ${projectKey} -r ${resource}`
       )
     })
 
     it('should log success messages', async () => {
-      results.forEach(result => {
-        stderr = result.stderr
-        stdout = result.stdout
-        expect(stderr).toBeFalsy()
-        expect(stdout).toMatch(/Starting to delete fetched resource/)
-        expect(stdout).toMatch(/All deleted/)
-      })
+      expect(stderr).toBeFalsy()
+      expect(stdout)
+        .toMatch(/Starting to delete fetched carts/)
+        .toMatch(/All deleted/)
     })
 
-    it('should delete resource', async () => {
+    it('should delete carts', async () => {
       const data = await fs.readFile(filePath, { encoding: 'utf8' })
-      expect(data).toBeDefined()
-      expect(data).toEqual('')
-      expect(data).toHaveLength(0)
+      expect(data)
+        .toBeDefined()
+        .toEqual('')
+        .toHaveLength(0)
+    })
+  })
+
+  describe('when deleting categories', () => {
+    let filePath
+    let stdout
+    let stderr
+    const resource = 'categories'
+
+    beforeAll(async () => {
+      filePath = tmp.fileSync().name
+      ;[stdout, stderr] = await exec(
+        `${bin} -o ${filePath} -p ${projectKey} -r ${resource}`
+      )
+    })
+
+    it('should log success messages', async () => {
+      expect(stderr).toBeFalsy()
+      expect(stdout)
+        .toMatch(/Starting to delete fetched categories/)
+        .toMatch(/All deleted/)
+    })
+
+    it('should delete categories', async () => {
+      const data = await fs.readFile(filePath, { encoding: 'utf8' })
+      expect(data)
+        .toBeDefined()
+        .toEqual('')
+        .toHaveLength(0)
+    })
+  })
+
+  describe('when deleting channels', () => {
+    let filePath
+    let stdout
+    let stderr
+    const resource = 'channels'
+
+    beforeAll(async () => {
+      filePath = tmp.fileSync().name
+      ;[stdout, stderr] = await exec(
+        `${bin} -o ${filePath} -p ${projectKey} -r ${resource}`
+      )
+    })
+
+    it('should log success messages', async () => {
+      expect(stderr).toBeFalsy()
+      expect(stdout)
+        .toMatch(/Starting to delete fetched channels/)
+        .toMatch(/All deleted/)
+    })
+
+    it('should delete channels', async () => {
+      const data = await fs.readFile(filePath, { encoding: 'utf8' })
+      expect(data)
+        .toBeDefined()
+        .toEqual('')
+        .toHaveLength(0)
+    })
+  })
+
+  describe('when deleting customerGroups', () => {
+    let filePath
+    let stdout
+    let stderr
+    const resource = 'customerGroups'
+
+    beforeAll(async () => {
+      filePath = tmp.fileSync().name
+      ;[stdout, stderr] = await exec(
+        `${bin} -o ${filePath} -p ${projectKey} -r ${resource}`
+      )
+    })
+
+    it('should log success messages', async () => {
+      expect(stderr).toBeFalsy()
+      expect(stdout)
+        .toMatch(/Starting to delete fetched customerGroups/)
+        .toMatch(/All deleted/)
+    })
+
+    it('should delete customerGroups', async () => {
+      const data = await fs.readFile(filePath, { encoding: 'utf8' })
+      expect(data)
+        .toBeDefined()
+        .toEqual('')
+        .toHaveLength(0)
+    })
+  })
+
+  describe('when deleting customers', () => {
+    let filePath
+    let stdout
+    let stderr
+    const resource = 'customers'
+
+    beforeAll(async () => {
+      filePath = tmp.fileSync().name
+      ;[stdout, stderr] = await exec(
+        `${bin} -o ${filePath} -p ${projectKey} -r ${resource}`
+      )
+    })
+
+    it('should log success messages', async () => {
+      expect(stderr).toBeFalsy()
+      expect(stdout)
+        .toMatch(/Starting to delete fetched customers/)
+        .toMatch(/All deleted/)
+    })
+
+    it('should delete customers', async () => {
+      const data = await fs.readFile(filePath, { encoding: 'utf8' })
+      expect(data)
+        .toBeDefined()
+        .toEqual('')
+        .toHaveLength(0)
+    })
+  })
+
+  describe('when deleting customObjects', () => {
+    let filePath
+    let stdout
+    let stderr
+    const resource = 'customObjects'
+
+    beforeAll(async () => {
+      filePath = tmp.fileSync().name
+      ;[stdout, stderr] = await exec(
+        `${bin} -o ${filePath} -p ${projectKey} -r ${resource}`
+      )
+    })
+
+    it('should log success messages', async () => {
+      expect(stderr).toBeFalsy()
+      expect(stdout)
+        .toMatch(/Starting to delete fetched customObjects/)
+        .toMatch(/All deleted/)
+    })
+
+    it('should delete customObjects', async () => {
+      const data = await fs.readFile(filePath, { encoding: 'utf8' })
+      expect(data)
+        .toBeDefined()
+        .toEqual('')
+        .toHaveLength(0)
+    })
+  })
+
+  describe('when deleting discountCodes', () => {
+    let filePath
+    let stdout
+    let stderr
+    const resource = 'discountCodes'
+
+    beforeAll(async () => {
+      filePath = tmp.fileSync().name
+      ;[stdout, stderr] = await exec(
+        `${bin} -o ${filePath} -p ${projectKey} -r ${resource}`
+      )
+    })
+
+    it('should log success messages', async () => {
+      expect(stderr).toBeFalsy()
+      expect(stdout)
+        .toMatch(/Starting to delete fetched discountCodes/)
+        .toMatch(/All deleted/)
+    })
+
+    it('should delete discountCodes', async () => {
+      const data = await fs.readFile(filePath, { encoding: 'utf8' })
+      expect(data)
+        .toBeDefined()
+        .toEqual('')
+        .toHaveLength(0)
+    })
+  })
+
+  describe('when deleting inventoryEntries', () => {
+    let filePath
+    let stdout
+    let stderr
+    const resource = 'inventory'
+
+    beforeAll(async () => {
+      filePath = tmp.fileSync().name
+      ;[stdout, stderr] = await exec(
+        `${bin} -o ${filePath} -p ${projectKey} -r ${resource}`
+      )
+    })
+
+    it('should log success messages', async () => {
+      expect(stderr).toBeFalsy()
+      expect(stdout)
+        .toMatch(/Starting to delete fetched inventory/)
+        .toMatch(/All deleted/)
+    })
+
+    it('should delete inventoryEntries', async () => {
+      const data = await fs.readFile(filePath, { encoding: 'utf8' })
+      expect(data)
+        .toBeDefined()
+        .toEqual('')
+        .toHaveLength(0)
+    })
+  })
+
+  describe('when deleting payments', () => {
+    let filePath
+    let stdout
+    let stderr
+    const resource = 'payments'
+
+    beforeAll(async () => {
+      filePath = tmp.fileSync().name
+      ;[stdout, stderr] = await exec(
+        `${bin} -o ${filePath} -p ${projectKey} -r ${resource}`
+      )
+    })
+
+    it('should log success messages', async () => {
+      expect(stderr).toBeFalsy()
+      expect(stdout)
+        .toMatch(/Starting to delete fetched payments/)
+        .toMatch(/All deleted/)
+    })
+
+    it('should delete payments', async () => {
+      const data = await fs.readFile(filePath, { encoding: 'utf8' })
+      expect(data)
+        .toBeDefined()
+        .toEqual('')
+        .toHaveLength(0)
+    })
+  })
+
+  describe('when deleting productDiscounts', () => {
+    let filePath
+    let stdout
+    let stderr
+    const resource = 'productDiscounts'
+
+    beforeAll(async () => {
+      filePath = tmp.fileSync().name
+      ;[stdout, stderr] = await exec(
+        `${bin} -o ${filePath} -p ${projectKey} -r ${resource}`
+      )
+    })
+
+    it('should log success messages', async () => {
+      expect(stderr).toBeFalsy()
+      expect(stdout)
+        .toMatch(/Starting to delete fetched productDiscounts/)
+        .toMatch(/All deleted/)
+    })
+
+    it('should delete productDiscounts', async () => {
+      const data = await fs.readFile(filePath, { encoding: 'utf8' })
+      expect(data)
+        .toBeDefined()
+        .toEqual('')
+        .toHaveLength(0)
+    })
+  })
+
+  describe('when deleting products', () => {
+    let filePath
+    let stdout
+    let stderr
+    const resource = 'products'
+
+    beforeAll(async () => {
+      filePath = tmp.fileSync().name
+      ;[stdout, stderr] = await exec(
+        `${bin} -o ${filePath} -p ${projectKey} -r ${resource}`
+      )
+    })
+
+    it('should log success messages', async () => {
+      expect(stderr).toBeFalsy()
+      expect(stdout)
+        .toMatch(/Starting to delete fetched products/)
+        .toMatch(/All deleted/)
+    })
+
+    it('should delete products', async () => {
+      const data = await fs.readFile(filePath, { encoding: 'utf8' })
+      expect(data)
+        .toBeDefined()
+        .toEqual('')
+        .toHaveLength(0)
+    })
+  })
+
+  describe('when deleting productTypes', () => {
+    let filePath
+    let stdout
+    let stderr
+    const resource = 'productTypes'
+
+    beforeAll(async () => {
+      filePath = tmp.fileSync().name
+      ;[stdout, stderr] = await exec(
+        `${bin} -o ${filePath} -p ${projectKey} -r ${resource}`
+      )
+    })
+
+    it('should log success messages', async () => {
+      expect(stderr).toBeFalsy()
+      expect(stdout)
+        .toMatch(/Starting to delete fetched productTypes/)
+        .toMatch(/All deleted/)
+    })
+
+    it('should delete productTypes', async () => {
+      const data = await fs.readFile(filePath, { encoding: 'utf8' })
+      expect(data)
+        .toBeDefined()
+        .toEqual('')
+        .toHaveLength(0)
+    })
+  })
+
+  describe('when deleting reviews', () => {
+    let filePath
+    let stdout
+    let stderr
+    const resource = 'reviews'
+
+    beforeAll(async () => {
+      filePath = tmp.fileSync().name
+      ;[stdout, stderr] = await exec(
+        `${bin} -o ${filePath} -p ${projectKey} -r ${resource}`
+      )
+    })
+
+    it('should log success messages', async () => {
+      expect(stderr).toBeFalsy()
+      expect(stdout)
+        .toMatch(/Starting to delete fetched reviews/)
+        .toMatch(/All deleted/)
+    })
+
+    it('should delete reviews', async () => {
+      const data = await fs.readFile(filePath, { encoding: 'utf8' })
+      expect(data)
+        .toBeDefined()
+        .toEqual('')
+        .toHaveLength(0)
+    })
+  })
+
+  describe('when deleting shippingMethods', () => {
+    let filePath
+    let stdout
+    let stderr
+    const resource = 'shippingMethods'
+
+    beforeAll(async () => {
+      filePath = tmp.fileSync().name
+      ;[stdout, stderr] = await exec(
+        `${bin} -o ${filePath} -p ${projectKey} -r ${resource}`
+      )
+    })
+
+    it('should log success messages', async () => {
+      expect(stderr).toBeFalsy()
+      expect(stdout)
+        .toMatch(/Starting to delete fetched shippingMethods/)
+        .toMatch(/All deleted/)
+    })
+
+    it('should delete shippingMethods', async () => {
+      const data = await fs.readFile(filePath, { encoding: 'utf8' })
+      expect(data)
+        .toBeDefined()
+        .toEqual('')
+        .toHaveLength(0)
+    })
+  })
+
+  describe('when deleting states', () => {
+    let filePath
+    let stdout
+    let stderr
+    const resource = 'states'
+
+    beforeAll(async () => {
+      filePath = tmp.fileSync().name
+      ;[stdout, stderr] = await exec(
+        `${bin} -o ${filePath} -p ${projectKey} -r ${resource}`
+      )
+    })
+
+    it('should log success messages', async () => {
+      expect(stderr).toBeFalsy()
+      expect(stdout).toMatch(/Starting to delete fetched states/)
+    })
+
+    it('should delete states', async () => {
+      const data = await fs.readFile(filePath, { encoding: 'utf8' })
+      expect(data)
+        .toBeDefined()
+        .toEqual('')
+        .toHaveLength(0)
+    })
+  })
+
+  describe('when deleting taxCategories', () => {
+    let filePath
+    let stdout
+    let stderr
+    const resource = 'taxCategories'
+
+    beforeAll(async () => {
+      filePath = tmp.fileSync().name
+      ;[stdout, stderr] = await exec(
+        `${bin} -o ${filePath} -p ${projectKey} -r ${resource}`
+      )
+    })
+
+    it('should log success messages', async () => {
+      expect(stderr).toBeFalsy()
+      expect(stdout)
+        .toMatch(/Starting to delete fetched taxCategories/)
+        .toMatch(/All deleted/)
+    })
+
+    it('should delete taxCategories', async () => {
+      const data = await fs.readFile(filePath, { encoding: 'utf8' })
+      expect(data)
+        .toBeDefined()
+        .toEqual('')
+        .toHaveLength(0)
+    })
+  })
+
+  describe('when deleting types', () => {
+    let filePath
+    let stdout
+    let stderr
+    const resource = 'types'
+
+    beforeAll(async () => {
+      filePath = tmp.fileSync().name
+      ;[stdout, stderr] = await exec(
+        `${bin} -o ${filePath} -p ${projectKey} -r ${resource}`
+      )
+    })
+
+    it('should log success messages', async () => {
+      expect(stderr).toBeFalsy()
+      expect(stdout)
+        .toMatch(/Starting to delete fetched types/)
+        .toMatch(/All deleted/)
+    })
+
+    it('should delete types', async () => {
+      const data = await fs.readFile(filePath, { encoding: 'utf8' })
+      expect(data)
+        .toBeDefined()
+        .toEqual('')
+        .toHaveLength(0)
+    })
+  })
+
+  describe('when deleting zones', () => {
+    let filePath
+    let stdout
+    let stderr
+    const resource = 'zones'
+
+    beforeAll(async () => {
+      filePath = tmp.fileSync().name
+      ;[stdout, stderr] = await exec(
+        `${bin} -o ${filePath} -p ${projectKey} -r ${resource}`
+      )
+    })
+
+    it('should log success messages', async () => {
+      expect(stderr).toBeFalsy()
+      expect(stdout)
+        .toMatch(/Starting to delete fetched zones/)
+        .toMatch(/All deleted/)
+    })
+
+    it('should delete zones', async () => {
+      const data = await fs.readFile(filePath, { encoding: 'utf8' })
+      expect(data)
+        .toBeDefined()
+        .toEqual('')
+        .toHaveLength(0)
     })
   })
 })
