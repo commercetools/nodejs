70903a6a27e169832d020159ee3d95d549d80a25
babanila <babanila@yahoo.com>
2018-12-03 17:59:55 +0100

    test: fix broken int test

--- integration-tests/cli/csv-parser-price.it.js
+++ integration-tests/cli/csv-parser-price.it.js
@@ -103,17 +103,17 @@ describe('CSV and CLI Tests', () => {
       const tmpFile = tmp.fileSync()
       const expectedError = 'Row length does not match headers'
       const csvFilePath = path.join(samplesFolder, 'faulty-sample.csv')
-
-      await expect(
-        exec(
+      try {
+        await exec(
           `${binPath} -p ${projectKey}  -i ${csvFilePath} --logFile ${
             tmpFile.name
           }`
         )
-      ).rejects.toThrowError(expectedError)
-
-      const data = await fs.readFile(tmpFile.name, { encoding: 'utf8' })
-      expect(data).toContain(expectedError)
+      } catch (error) {
+        expect(String(error)).toMatch(expectedError)
+        const data = await fs.readFile(tmpFile.name, { encoding: 'utf8' })
+        expect(data).toContain(expectedError)
+      }
     })
   })
 
