bacd206c2b9d057905114dca5e3161c44d47fbf4
abeniasaad <achraf.beniasaad@commercetools.de>
2019-07-09 15:14:01 +0200

    adding necessary files

--- packages/typescript-sdk/src/ctp-client/ctp-client.ts
+++ packages/typescript-sdk/src/ctp-client/ctp-client.ts
@@ -1,10 +1,8 @@
-import { ApiRoot } from "../gen/client/ApiRoot";
-import { middwareFromCtpClient } from "../necessary-middlewares/ctp_middlware";
+import { ApiRoot } from '../gen/client/ApiRoot'
+import { middwareFromCtpClient } from '../necessary-middlewares/ctp_middlware'
 
-
-
-  export function createApiBuilderFromCtpClient(ctpClient): ApiRoot {
-      return new ApiRoot({
-          middlewares:[middwareFromCtpClient(ctpClient)]
-      })
-  }
+export function createApiBuilderFromCtpClient(ctpClient): ApiRoot {
+  return new ApiRoot({
+    middlewares: [middwareFromCtpClient(ctpClient)],
+  })
+}
--- packages/typescript-sdk/src/necessary-middlewares/ctp_middlware.ts
+++ packages/typescript-sdk/src/necessary-middlewares/ctp_middlware.ts
@@ -1,47 +1,47 @@
-import { Middleware, MiddlewareArg } from "../gen/base/common-types";
+import { Middleware, MiddlewareArg } from '../gen/base/common-types'
 
 export function middwareFromCtpClient(client): Middleware {
   return async (midlllewareArg: MiddlewareArg) =>
-    handleRequest(client, midlllewareArg);
+    handleRequest(client, midlllewareArg)
 }
 
 async function handleRequest(
   client,
   middlwareArg: MiddlewareArg
 ): Promise<MiddlewareArg> {
-  const { request } = middlwareArg;
+  const { request } = middlwareArg
 
   const modifiedRequest = {
     ...request,
-    uri: removeBaseUrl(request.uri)
-  };
+    uri: removeBaseUrl(request.uri),
+  }
 
   try {
-    const result = await client.execute(modifiedRequest);
-    const { uri, error, ...response } = result;
+    const result = await client.execute(modifiedRequest)
+    const { uri, error, ...response } = result
 
     return middlwareArg.next({
       ...middlwareArg,
       response,
-      error
-    });
+      error,
+    })
   } catch (error) {
     return middlwareArg.next({
       ...middlwareArg,
-      error
-    });
+      error,
+    })
   }
 }
 
 function removeBaseUrl(url) {
-  var baseUrlPattern = /^https?:\/\/[a-z\:0-9.]+/;
-  var result = "";
-  var match = baseUrlPattern.exec(url);
+  var baseUrlPattern = /^https?:\/\/[a-z\:0-9.]+/
+  var result = ''
+  var match = baseUrlPattern.exec(url)
   if (match != null) {
-    result = match[0];
+    result = match[0]
   }
   if (result.length > 0) {
-    url = url.replace(result, "");
+    url = url.replace(result, '')
   }
-  return url;
+  return url
 }
