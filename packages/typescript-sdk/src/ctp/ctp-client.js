c0720f2ccba491a9f05a7244584992d8fed07516
abeniasaad <achraf.beniasaad@commercetools.de>
2019-11-25 11:18:33 +0100

    remove unecessary files

--- packages/typescript-sdk/src/ctp/ctp-client.js
+++
@@ -1,8 +0,0 @@
-import { middlewareFromCtpClient } from './ctp-middleware';
-import { ApiRoot } from '../generated/client/api-root';
-export function createApiBuilderFromCtpClient(ctpClient) {
-    return new ApiRoot({
-        middlewares: [middlewareFromCtpClient(ctpClient)],
-    });
-}
-//# sourceMappingURL=ctp-client.js.map
