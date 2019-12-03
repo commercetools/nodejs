d6930c57fd2f75402b47e1f2443b319d4f1cdd93
amin <amin.benselim@commercetools.de>
2019-12-03 15:54:36 +0100

    feat: add setLanguages update action

--- packages/sync-actions/src/stores-actions.js
+++ packages/sync-actions/src/stores-actions.js
@@ -1,6 +1,9 @@
 import { buildBaseAttributesActions } from './utils/common-actions'
 
-export const baseActionsList = [{ action: 'setName', key: 'name' }]
+export const baseActionsList = [
+  { action: 'setName', key: 'name' },
+  { action: 'setLanguages', key: 'languages' },
+]
 
 export function actionsMapBase(diff, oldObj, newObj) {
   return buildBaseAttributesActions({
