0ed958e97e7d3e1bb21f0c9e1e1ef348bf9de5ae
abeniasaad <achraf.beniasaad@commercetools.de>
2020-01-23 09:54:45 +0100

    roll back

--- packages/typescript-sdk/test/generated/base/request-utils.spec.ts
+++ packages/typescript-sdk/test/generated/base/request-utils.spec.ts
@@ -1,8 +1,8 @@
-import { ApiRequestExecutor } from 'shared/utils/requests-utils'
+import { ApiRequestExecutor } from '../../../src/generated/shared/utils/requests-utils'
 import {
   MiddlewareArg,
   VariableMap,
-} from 'shared/utils/common-types'
+} from '../../../src/generated/shared/utils/common-types'
 import * as url from 'url'
 
 describe('ApiRequestExecutor', () => {
