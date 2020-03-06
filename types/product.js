8d30aea4120a7f99088eaa600c2cdbbab1166eef
vineet kumar <javascripter.pune@outlook.com>
2020-03-06 11:52:11 +0400

    feat(product-exporter): add productTypeKey column while exporting

--- types/product.js
+++ types/product.js
@@ -344,6 +344,7 @@ export type MappedProduct = {
   createdAt?: string,
   lastModifiedAt?: string,
   productType?: string,
+  productTypeKey?: string,
   name?: Object,
   description?: Object,
   slug?: Object,
