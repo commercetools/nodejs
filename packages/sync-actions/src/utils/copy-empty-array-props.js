555c0118f0d30f54d56133c8b7128de631ff4329
Markus Gihady <markusgihady@gmail.com>
2020-04-15 21:53:49 +0200

    improvement(sync-actions): copy empty array props without mutating values

--- packages/sync-actions/src/utils/copy-empty-array-props.js
+++ packages/sync-actions/src/utils/copy-empty-array-props.js
@@ -1,26 +1,33 @@
 const deepCopyObj = (obj) => JSON.parse(JSON.stringify(obj))
 
 const copyEmptyArrayPropsRecursion = (oldObjCopy, newObjCopy) => {
-  Object.entries(oldObjCopy).forEach(([key, value]) => {
-    if (Array.isArray(value)) {
-      const foundKey = Object.keys(newObjCopy).findIndex(
-        (newObjCopyKey) => JSON.stringify(newObjCopyKey) === JSON.stringify(key)
-      )
+  const newObjCopyOne = Object.entries(oldObjCopy).reduce(
+    (acc, [key, value]) => {
+      // check if the value is array on `oldObjCopy`
+      if (Array.isArray(value)) {
+        const foundKey = Object.keys(acc).findIndex(
+          (accKey) => JSON.stringify(accKey) === JSON.stringify(key)
+        )
 
-      if (foundKey === -1) {
-        newObjCopy[key] = [] // eslint-disable-line no-param-reassign
+        // if the key not found in `acc` (newObjectCopy)
+        if (foundKey === -1) {
+          return { ...acc, [key]: [] }
+        }
+      } else if (typeof value === 'object' && acc[key]) {
+        // recursion, so we could check for nested objects
+        const returnedNewObjCopy = copyEmptyArrayPropsRecursion(
+          value,
+          acc[key]
+        )[1]
+        return { ...acc, [key]: returnedNewObjCopy }
       }
-    } else if (
-      typeof value === 'object' &&
-      oldObjCopy[key] &&
-      newObjCopy[key]
-    ) {
-      // recursion, so we could check for nested objects
-      copyEmptyArrayPropsRecursion(oldObjCopy[key], newObjCopy[key])
-    }
-  })
 
-  return [oldObjCopy, newObjCopy]
+      return acc
+    },
+    { ...newObjCopy }
+  )
+
+  return [oldObjCopy, newObjCopyOne]
 }
 
 /**
