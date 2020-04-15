979d84b10e070c6f59841ac8dfd864838f5007c4
Markus Gihady <markusgihady@gmail.com>
2020-04-15 23:46:02 +0200

    improvement(sync-actions): remove func which not used, copy empty array props

--- packages/sync-actions/src/utils/copy-empty-array-props.js
+++ packages/sync-actions/src/utils/copy-empty-array-props.js
@@ -1,35 +1,3 @@
-const deepCopyObj = (obj) => JSON.parse(JSON.stringify(obj))
-
-const copyEmptyArrayPropsRecursion = (oldObjCopy, newObjCopy) => {
-  const newObjCopyOne = Object.entries(oldObjCopy).reduce(
-    (acc, [key, value]) => {
-      // check if the value is array on `oldObjCopy`
-      if (Array.isArray(value)) {
-        const foundKey = Object.keys(acc).findIndex(
-          (accKey) => JSON.stringify(accKey) === JSON.stringify(key)
-        )
-
-        // if the key not found in `acc` (newObjectCopy)
-        if (foundKey === -1) {
-          return { ...acc, [key]: [] }
-        }
-      } else if (typeof value === 'object' && acc[key]) {
-        // recursion, so we could check for nested objects
-        const returnedNewObjCopy = copyEmptyArrayPropsRecursion(
-          value,
-          acc[key]
-        )[1]
-        return { ...acc, [key]: returnedNewObjCopy }
-      }
-
-      return acc
-    },
-    { ...newObjCopy }
-  )
-
-  return [oldObjCopy, newObjCopyOne]
-}
-
 /**
  * @function copyEmptyArrayProps
  * @description Takes two objects and if there are Array props in oldObj which doesn't exist in newObj, then copy it with an empty value.
@@ -39,8 +7,28 @@ const copyEmptyArrayPropsRecursion = (oldObjCopy, newObjCopy) => {
  */
 
 export default function copyEmptyArrayProps(oldObj, newObj) {
-  const oldObjCopy = deepCopyObj(oldObj)
-  const newObjCopy = deepCopyObj(newObj)
+  const newObjCopy = Object.entries(oldObj).reduce(
+    (acc, [key, value]) => {
+      // check if the value is array on `oldObj`
+      if (Array.isArray(value)) {
+        const foundKey = Object.keys(acc).findIndex(
+          (accKey) => JSON.stringify(accKey) === JSON.stringify(key)
+        )
 
-  return copyEmptyArrayPropsRecursion(oldObjCopy, newObjCopy)
+        // if the key not found in `acc` (newObjectCopy), init `key` with empty array
+        if (foundKey === -1) {
+          return { ...acc, [key]: [] }
+        }
+      } else if (typeof value === 'object' && acc[key]) {
+        // recursion, so we could check for nested objects
+        const returnedNewObjCopy = copyEmptyArrayProps(value, acc[key])[1]
+        return { ...acc, [key]: returnedNewObjCopy }
+      }
+
+      return acc
+    },
+    { ...newObj }
+  )
+
+  return [oldObj, newObjCopy]
 }
