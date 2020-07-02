5604fb595ddf7890b2e95a4e433b1bd14137d12b
Markus Gihady <markusgihady@gmail.com>
2020-07-02 18:42:50 +0200

    feat(sync-actions): add array check for copyEmptyArrayProps

--- packages/sync-actions/src/utils/copy-empty-array-props.js
+++ packages/sync-actions/src/utils/copy-empty-array-props.js
@@ -15,13 +15,49 @@ export default function copyEmptyArrayProps(oldObj = {}, newObj = {}) {
           ...newObj,
           ...nextObject,
         }
+
+        if (key === 'custom') {
+          return {
+            ...merged,
+          }
+        }
+
+        if (Array.isArray(value) && newObj[key] && newObj[key].length >= 1) {
+          /* eslint-disable no-plusplus */
+          for (let i = 0; i < newObj[key].length; i++) {
+            if (
+              !isNil(newObj[key][i]) &&
+              typeof newObj[key][i] === 'object' &&
+              !isNil(newObj[key][i].id)
+            ) {
+              /* eslint-disable eqeqeq */
+              const foundObject = value.find((v) => v.id == newObj[key][i].id)
+              if (!isNil(foundObject)) {
+                const [, nestedObject] = copyEmptyArrayProps(
+                  foundObject,
+                  newObj[key][i]
+                )
+                /* eslint-disable no-param-reassign */
+                newObj[key][i] = nestedObject
+              }
+            }
+          }
+
+          return {
+            ...merged,
+          }
+        }
         if (Array.isArray(value)) {
           return {
             ...merged,
             [key]: isNil(newObj[key]) ? [] : newObj[key],
           }
         }
-        if (!isNil(newObj[key]) && typeof value === 'object') {
+        if (
+          !isNil(newObj[key]) &&
+          typeof value === 'object' &&
+          !(value instanceof Date)
+        ) {
           const [, nestedObject] = copyEmptyArrayProps(value, newObj[key])
           return {
             ...merged,
