1aef3423e96da7f5df20fd5f66ec29146cacee83
Mohibullah Amin Wardak <mohib0306.w@gmail.com>
2022-02-23 13:15:30 +0100

    feat(sync-actions): add product selection sync actions (#1767)
    * feat(sync-actions): add product selection sync actions
    
    * chore(changeset): add changeset release

---
+++ packages/sync-actions/src/product-selections.js
@@ -0,0 +1,55 @@
+/* @flow */
+import flatten from 'lodash.flatten'
+import type { SyncAction, UpdateAction, ActionGroup } from 'types/sdk'
+import createBuildActions from './utils/create-build-actions'
+import createMapActionGroup from './utils/create-map-action-group'
+import actionsMapCustom from './utils/action-map-custom'
+import * as productSelectionsActions from './product-selections-actions'
+import * as diffpatcher from './utils/diffpatcher'
+
+export const actionGroups = ['base']
+
+function createProductSelectionsMapActions(
+  mapActionGroup: (
+    type: string,
+    fn: () => Array<UpdateAction>
+  ) => Array<UpdateAction>
+): (
+  diff: Object,
+  next: Object,
+  previous: Object,
+  options: Object
+) => Array<UpdateAction> {
+  return function doMapActions(
+    diff: Object,
+    next: Object,
+    previous: Object
+  ): Array<UpdateAction> {
+    const allActions = []
+    allActions.push(
+      mapActionGroup('base', (): Array<UpdateAction> =>
+        productSelectionsActions.actionsMapBase(diff, previous, next)
+      )
+    )
+    allActions.push(
+      mapActionGroup('custom', (): Array<UpdateAction> =>
+        actionsMapCustom(diff, next, previous)
+      )
+    )
+
+    return flatten(allActions)
+  }
+}
+
+export default (actionGroupList: Array<ActionGroup>): SyncAction => {
+  const mapActionGroup = createMapActionGroup(actionGroupList)
+  const doMapActions = createProductSelectionsMapActions(mapActionGroup)
+  const onBeforeApplyingDiff = null
+  const buildActions = createBuildActions(
+    diffpatcher.diff,
+    doMapActions,
+    onBeforeApplyingDiff
+  )
+
+  return { buildActions }
+}
