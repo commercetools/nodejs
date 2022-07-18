58f69c34561da1e18999aabdbc39bca6dd59a2d6
Chukwuemeka Ajima <meeky.ae@gmail.com>
2022-07-18 23:26:56 +0200

    chore(tests): add tests to improve coverage report

--- packages/sync-actions/test/utils/create-build-array-actions.spec.js
+++ packages/sync-actions/test/utils/create-build-array-actions.spec.js
@@ -1,4 +1,4 @@
-import { diff } from '../../src/utils/diffpatcher'
+import { diff, getDeltaValue } from '../../src/utils/diffpatcher'
 import createBuildArrayActions, {
   ADD_ACTIONS,
   REMOVE_ACTIONS,
@@ -74,4 +74,80 @@ describe('createBuildArrayActions', () => {
 
     expect(removeActionSpy).toHaveBeenCalledWith(expect.any(Object), 0)
   })
+
+  test('should throw an error for non array parameter', () => {
+    expect(() => getDeltaValue('non-array-imput', { sample: 'object' })).toThrow()
+  });
+  test('throw an error if `originalObject` is not provided', () => {
+    const sampleArray = [
+      {
+        measurements: {
+          heightInMillimeter: 10,
+          lengthInMillimeter: 20,
+          widthInMillimeter: 2,
+          weightInGram: 5
+        },
+        trackingData: { trackingId: 'tracking-id-1' }
+      },
+      {
+        measurements: {
+          heightInMillimeter: 10,
+          lengthInMillimeter: 20,
+          widthInMillimeter: 2,
+          weightInGram: 5
+        },
+        trackingData: { trackingId: 'tracking-id-2' }
+      },
+      2
+    ]
+    expect(() => getDeltaValue(sampleArray, null)).toThrow()
+  });
+  test('throw an error if array is length 3 and second item is 3', () => {
+    const sampleArray = [
+      {
+        measurements: {
+          heightInMillimeter: 10,
+          lengthInMillimeter: 20,
+          widthInMillimeter: 2,
+          weightInGram: 5
+        },
+        trackingData: { trackingId: 'tracking-id-1' }
+      },
+      {
+        measurements: {
+          heightInMillimeter: 10,
+          lengthInMillimeter: 20,
+          widthInMillimeter: 2,
+          weightInGram: 5
+        },
+        trackingData: { trackingId: 'tracking-id-2' }
+      },
+      3
+    ]
+    expect(() => getDeltaValue(sampleArray, null)).toThrow()
+  });
+  test('throw an error if array is length 3 and second item is 4', () => {
+    const sampleArray = [
+      {
+        measurements: {
+          heightInMillimeter: 10,
+          lengthInMillimeter: 20,
+          widthInMillimeter: 2,
+          weightInGram: 5
+        },
+        trackingData: { trackingId: 'tracking-id-1' }
+      },
+      {
+        measurements: {
+          heightInMillimeter: 10,
+          lengthInMillimeter: 20,
+          widthInMillimeter: 2,
+          weightInGram: 5
+        },
+        trackingData: { trackingId: 'tracking-id-2' }
+      },
+      4
+    ]
+    expect(() => getDeltaValue(sampleArray, null)).toThrow()
+  });
 })
