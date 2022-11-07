d493e9faf1138e03074f5d29e81d3552c406d573
Levente Kóbor <levente.kobor@commercetools.com>
2022-11-07 16:06:18 +0100

    feat(sync-actions): add support for quotes

---
+++ packages/sync-actions/src/quotes.js
@@ -0,0 +1,37 @@
+import flatten from 'lodash.flatten'
+import createBuildActions from './utils/create-build-actions'
+import createMapActionGroup from './utils/create-map-action-group'
+import actionsMapCustom from './utils/action-map-custom'
+import { actionsMapBase } from './quotes-actions'
+import combineValidityActions from './utils/combine-validity-actions'
+import * as diffpatcher from './utils/diffpatcher'
+
+export const actionGroups = ['base', 'custom']
+
+function createQuotesMapActions(mapActionGroup, syncActionConfig) {
+  return function doMapActions(diff, newObj, oldObj) {
+    const allActions = []
+
+    allActions.push(
+      mapActionGroup('base', () =>
+        actionsMapBase(diff, oldObj, newObj, syncActionConfig)
+      )
+    )
+
+    allActions.push(
+      mapActionGroup('custom', () => actionsMapCustom(diff, newObj, oldObj))
+    )
+
+    return combineValidityActions(flatten(allActions))
+  }
+}
+
+export default (actionGroupList, syncActionConfig = {}) => {
+  const mapActionGroup = createMapActionGroup(actionGroupList)
+  const doMapActions = createQuotesMapActions(
+    mapActionGroup,
+    syncActionConfig
+  )
+  const buildActions = createBuildActions(diffpatcher.diff, doMapActions)
+  return { buildActions }
+}
