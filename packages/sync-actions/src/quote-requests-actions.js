798da21ba8dbdf14dff1812d07c47f4392b7153d
Levente Kóbor <levente.kobor@commercetools.com>
2022-11-08 22:38:39 +0100

    feat(sync-action): add support for quote requests

---
+++ packages/sync-actions/src/quote-requests-actions.js
@@ -0,0 +1,16 @@
+import { buildBaseAttributesActions } from './utils/common-actions'
+
+export const baseActionsList = [
+  { action: 'changeQuoteRequestState', key: 'quoteRequestState' },
+  { action: 'transitionState', key: 'state'},
+]
+
+export function actionsMapBase(diff, oldObj, newObj, config = {}) {
+  return buildBaseAttributesActions({
+    actions: baseActionsList,
+    diff,
+    oldObj,
+    newObj,
+    shouldOmitEmptyString: config.shouldOmitEmptyString,
+  })
+}
