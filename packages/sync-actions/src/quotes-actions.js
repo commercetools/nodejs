b07dbae39301a00e758a682412c9ab18b43b9bc5
Levente <levente.kobor94@gmail.com>
2022-11-10 09:17:10 +0100

    feat(sync-actions): add support for quotes (#1813)
    * feat(sync-actions): add support for quotes
    
    * fix(sync-actions): add flow annotation
    
    * feat(sync-actions): add support for quotes

---
+++ packages/sync-actions/src/quotes-actions.js
@@ -0,0 +1,17 @@
+import { buildBaseAttributesActions } from './utils/common-actions'
+
+export const baseActionsList = [
+  { action: 'changeQuoteState', key: 'quoteState' },
+  { action: 'requestQuoteRenegotiation', key: 'buyerComment' },
+  { action: 'transitionState', key: 'state'},
+]
+
+export function actionsMapBase(diff, oldObj, newObj, config = {}) {
+  return buildBaseAttributesActions({
+    actions: baseActionsList,
+    diff,
+    oldObj,
+    newObj,
+    shouldOmitEmptyString: config.shouldOmitEmptyString,
+  })
+}
