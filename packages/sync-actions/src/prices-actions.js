9355ee8dfd0c67591ab83d462e99cae5afa74b26
Mario Arriola <mario.arriola@commercetools.com>
2023-06-26 15:52:11 +0200

    feat: add missing sync action for StandalonePrice entity setPriceTiers

--- packages/sync-actions/src/prices-actions.js
+++ packages/sync-actions/src/prices-actions.js
@@ -1,12 +1,10 @@
 /* eslint-disable max-len */
-import {
-  buildBaseAttributesActions,
-} from './utils/common-actions'
-
+import { buildBaseAttributesActions } from './utils/common-actions'
 
 export const baseActionsList = [
   { action: 'changeValue', key: 'value' },
   { action: 'setDiscountedPrice', key: 'discounted' },
+  { action: 'setPriceTiers', key: 'tiers' },
 ]
 
 export function actionsMapBase(diff, oldObj, newObj, config = {}) {
@@ -18,4 +16,3 @@ export function actionsMapBase(diff, oldObj, newObj, config = {}) {
     shouldOmitEmptyString: config.shouldOmitEmptyString,
   })
 }
-
