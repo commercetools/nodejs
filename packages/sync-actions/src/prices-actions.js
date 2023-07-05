7285a9fbcbcfca6a9460e36ba7b58bb30f34fac6
Mario Arriola <64659499+ARRIOLALEO@users.noreply.github.com>
2023-07-05 08:50:35 +0200

    Ma fix sync set price tier (#1869)
    * fix: typo priceTiers correct priceTier
    
    * chore: run yarn changeset
    
    ---------
    
    Co-authored-by: Mario Arriola <mario.arriola@commercetools.com>

--- packages/sync-actions/src/prices-actions.js
+++ packages/sync-actions/src/prices-actions.js
@@ -4,7 +4,7 @@ export const baseActionsList = [
   { action: 'changeValue', key: 'value' },
   { action: 'setDiscountedPrice', key: 'discounted' },
   // TODO: Later add more accurate actions `addPriceTier`, `removePriceTier`
-  { action: 'setPriceTiers', key: 'tiers' },
+  { action: 'setPriceTier', key: 'tiers' },
   { action: 'setKey', key: 'key' },
   { action: 'setValidFrom', key: 'validFrom' },
   { action: 'setValidUntil', key: 'validUntil' },
