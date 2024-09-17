257dd77f901fa49ca8a0a92c9b8dde9ff636a3b4
antoniolodias <antonio.dias@commercetools.com>
2024-09-17 15:15:31 +0100

    fix: remove typename from subrates as well

--- packages/sync-actions/src/tax-categories-actions.js
+++ packages/sync-actions/src/tax-categories-actions.js
@@ -22,6 +22,11 @@ export function actionsMapBase(diff, oldObj, newObj, config = {}) {
   })
 }
 
+function removeTypename(obj) {
+  const { __typename, ...objWithoutTypename } = obj
+  return objWithoutTypename
+}
+
 export function actionsMapRates(diff, oldObj, newObj) {
   const handler = createBuildArrayActions('rates', {
     [ADD_ACTIONS]: (newObject) => ({
@@ -35,7 +40,16 @@ export function actionsMapRates(diff, oldObj, newObj) {
     [CHANGE_ACTIONS]: (oldObject, updatedObject) => {
       // filter out taxRates that were not changed
       // so the API doesn't return it with a different id
-      const { __typename, ...oldObjectWithoutTypename } = oldObject
+      // we need to remove __typename from the object to compare them
+      const taxCategoryWithoutTypeName = removeTypename(oldObject)
+      const oldObjectSubRatesWithoutTypename =
+        oldObject.subRates?.map(removeTypename)
+
+      const oldObjectWithoutTypename = {
+        ...taxCategoryWithoutTypeName,
+        subRates: oldObjectSubRatesWithoutTypename,
+      }
+
       if (deepEqual(oldObjectWithoutTypename, updatedObject)) return null
 
       return {
